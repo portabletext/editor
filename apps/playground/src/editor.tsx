b5257b7d3de0909b01d27f1e9a7f129dc177b03a
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-06-25 11:54:59 +0200

    chore(playground): show spinner when editor is loading

--- apps/playground/src/editor.tsx
+++ apps/playground/src/editor.tsx
@@ -13,46 +13,55 @@ import {PortableTextBlock} from '@sanity/types'
 import {schema} from './schema'
 import {useState} from 'react'
 import {applyAll} from '@portabletext/patches/apply'
-import {Code, Stack} from '@sanity/ui'
+import {Box, Code, Flex, Spinner, Stack} from '@sanity/ui'
 import {wait} from './wait'
 
 export function Editor() {
+  const [loading, setLoading] = useState(false)
   const [value, setValue] = useState<Array<PortableTextBlock>>([])
 
   return (
     <Stack space={2}>
-      <PortableTextEditor
-        onChange={(change) => {
-          if (change.type === 'mutation') {
-            setValue(applyAll(value, change.patches))
-          }
-        }}
-        schemaType={schema}
-      >
-        <PortableTextEditable
-          onPaste={(data) => {
-            const text = data.event.clipboardData.getData('text')
-            if (text === 'heading') {
-              return wait(2000).then(() => ({
-                insert: [
-                  {
-                    _type: 'block',
-                    children: [{_type: 'span', text: 'heading'}],
-                    style: 'h1',
-                  },
-                ],
-              }))
-            }
-          }}
-          renderAnnotation={renderAnnotation}
-          renderBlock={renderBlock}
-          renderChild={renderChild}
-          renderDecorator={renderDecorator}
-          renderListItem={renderListItem}
-          renderPlaceholder={renderPlaceholder}
-          renderStyle={renderStyle}
-        />
-      </PortableTextEditor>
+      <Flex gap={2} align="center">
+        <Box flex={1}>
+          <PortableTextEditor
+            onChange={(change) => {
+              if (change.type === 'mutation') {
+                setValue(applyAll(value, change.patches))
+              }
+              if (change.type === 'loading') {
+                setLoading(change.isLoading)
+              }
+            }}
+            schemaType={schema}
+          >
+            <PortableTextEditable
+              onPaste={(data) => {
+                const text = data.event.clipboardData.getData('text')
+                if (text === 'heading') {
+                  return wait(2000).then(() => ({
+                    insert: [
+                      {
+                        _type: 'block',
+                        children: [{_type: 'span', text: 'heading'}],
+                        style: 'h1',
+                      },
+                    ],
+                  }))
+                }
+              }}
+              renderAnnotation={renderAnnotation}
+              renderBlock={renderBlock}
+              renderChild={renderChild}
+              renderDecorator={renderDecorator}
+              renderListItem={renderListItem}
+              renderPlaceholder={renderPlaceholder}
+              renderStyle={renderStyle}
+            />
+          </PortableTextEditor>
+        </Box>
+        {loading ? <Spinner /> : null}{' '}
+      </Flex>
       <Code as="code" size={0} language="json">
         {JSON.stringify(value, null, 2)}
       </Code>
