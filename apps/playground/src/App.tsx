237f0f075556cb3335e88dbfa81dbb66fb13748d
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-06-27 13:54:24 +0200

    chore(playground): improve global styling

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,11 +1,19 @@
-import {ThemeProvider} from '@sanity/ui'
+import {Card, ThemeProvider, usePrefersDark} from '@sanity/ui'
 import {buildTheme} from '@sanity/ui/theme'
 import {Editor} from './editor'
+import {GlobalStyle} from './global-style'
+
+const theme = buildTheme()
 
 export function App() {
+  const prefersDark = usePrefersDark()
+
   return (
-    <ThemeProvider theme={buildTheme()}>
-      <Editor />
+    <ThemeProvider theme={theme} scheme={prefersDark ? 'dark' : 'light'}>
+      <GlobalStyle />
+      <Card height="fill">
+        <Editor />
+      </Card>
     </ThemeProvider>
   )
 }
