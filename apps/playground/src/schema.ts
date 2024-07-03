ea6b2872f62c44998b5d0aee8493a6cc51055a64
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-07-03 14:04:17 +0200

    chore(playground): add very simple annotation example

--- apps/playground/src/schema.ts
+++ apps/playground/src/schema.ts
@@ -1,10 +1,30 @@
-export const schema = {
+import {z} from 'zod'
+import {ArrayDefinition} from '@sanity/types'
+
+export const schema: ArrayDefinition = {
   type: 'array' as const,
   name: 'body',
   of: [
     {
-      type: 'block' as const,
+      type: 'block',
       name: 'block',
+      marks: {
+        annotations: [
+          {
+            name: 'link',
+            type: 'object',
+          },
+        ],
+      },
     },
   ],
 }
+
+export const LinkAnnotationSchema = z.object({
+  schemaType: z.object({
+    name: z.literal('link'),
+  }),
+  value: z.object({
+    href: z.string(),
+  }),
+})
