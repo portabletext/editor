e41ea1065924a5466624c160afb06ce1d10ae358
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-07-04 11:51:43 +0200

    chore(playground): disable react-refresh/only-export-components

--- apps/playground/.eslintrc.cjs
+++ apps/playground/.eslintrc.cjs
@@ -10,6 +10,6 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
+    'react-refresh/only-export-components': ['off'],
   },
 }
