ba50de7d46cf7adea0d70fca441c97bd9fa8cb31
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-07-29 15:49:52 +0200

    feat(playground): add error boundary around PortableTextEditable

---
+++ apps/playground/src/components/error-screen.tsx
@@ -0,0 +1,20 @@
+import {isError} from 'remeda'
+import {Button} from './button'
+
+export function ErrorScreen(props: {area: string; error: Error; dismiss: () => void}) {
+  return (
+    <div className="bg-red-100 text-red-900 flex flex-col gap-2 p-2">
+      <div className="font-medium">
+        The <code className="font-bold">{props.area}</code> crashed!
+      </div>
+      <div className="font-mono text-sm">{getErrorMessage(props.error, 'Unknown error')}</div>
+      <Button variant="destructive" className="self-start" size="sm" onPress={props.dismiss}>
+        Try again
+      </Button>
+    </div>
+  )
+}
+
+export function getErrorMessage(error: unknown, fallback: string) {
+  return isError(error) ? error.message : fallback
+}
