f528358d43c20d2f89c986061843a5180250c9a0
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-08-06 11:29:39 +0200

    chore(editor): suppress linting error

--- packages/editor/test-config/jsdom.jest.env.ts
+++ packages/editor/test-config/jsdom.jest.env.ts
@@ -4,6 +4,7 @@ export default class JSDOMEnvironmentWithDomRange extends JSDOMEnvironment {
   public async setup(): Promise<void> {
     await super.setup()
     if (typeof this.global.TextEncoder === 'undefined') {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
       const {TextEncoder, TextDecoder} = require('node:util')
       this.global.TextEncoder = TextEncoder
       this.global.TextDecoder = TextDecoder
