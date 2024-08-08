e4d5d3a21744fdd274ea8994c7cb5989c0b59723
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-08-08 12:03:09 +0200

    test(editor): make it possible to annotate text in e2e tests

--- packages/editor/e2e-tests/schema.ts
+++ packages/editor/e2e-tests/schema.ts
@@ -11,24 +11,34 @@ export const someObject = {
   fields: [{type: 'string', name: 'color'}],
 }
 
-export const blockType = {
-  type: 'block',
-  name: 'block',
-  styles: [
-    {title: 'Normal', value: 'normal'},
-    {title: 'H1', value: 'h1'},
-    {title: 'H2', value: 'h2'},
-    {title: 'H3', value: 'h3'},
-    {title: 'H4', value: 'h4'},
-    {title: 'H5', value: 'h5'},
-    {title: 'H6', value: 'h6'},
-    {title: 'Quote', value: 'blockquote'},
-  ],
-  of: [someObject],
-}
-
 export const portableTextType = defineType({
   type: 'array',
   name: 'body',
-  of: [blockType, someObject],
+  of: [
+    {
+      type: 'block',
+      name: 'block',
+      styles: [
+        {title: 'Normal', value: 'normal'},
+        {title: 'H1', value: 'h1'},
+        {title: 'H2', value: 'h2'},
+        {title: 'H3', value: 'h3'},
+        {title: 'H4', value: 'h4'},
+        {title: 'H5', value: 'h5'},
+        {title: 'H6', value: 'h6'},
+        {title: 'Quote', value: 'blockquote'},
+      ],
+      marks: {
+        annotations: [
+          {
+            name: 'comment',
+            type: 'object',
+            fields: [{type: 'string', name: 'text'}],
+          },
+        ],
+      },
+      of: [someObject],
+    },
+    someObject,
+  ],
 })
