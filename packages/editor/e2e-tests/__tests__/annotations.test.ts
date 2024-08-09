cd6bed80ded6b02f08dd8faf9b72ed4725614f1a
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-08-09 17:36:36 +0200

    test(editor): add test for redoing the deletion of annotated text

--- packages/editor/e2e-tests/__tests__/annotations.test.ts
+++ packages/editor/e2e-tests/__tests__/annotations.test.ts
@@ -48,6 +48,50 @@ describe('Feature: Annotations', () => {
     expect(span?.marks).toEqual([commentKey])
   })
 
+  it('Scenario: Redoing the deletion of the last char of annotated text', async () => {
+    const [editorA] = await getEditors()
+
+    // Given the text "foo"
+    await editorA.insertText('foo')
+
+    // And a "comment" around the text
+    await editorA.setSelection({
+      anchor: {path: [{_key: 'A-4'}, 'children', {_key: 'A-3'}], offset: 0},
+      focus: {path: [{_key: 'A-4'}, 'children', {_key: 'A-3'}], offset: 3},
+    })
+    await editorA.toggleMark('m')
+    const value = await editorA.getValue()
+    const block = value && isPortableTextBlock(value[0]) ? value[0] : undefined
+    const commentKey = block?.markDefs?.[0]?._key
+    const spanKey = block?.children[0]?._key
+
+    // When "Backspace" is pressed once
+    await editorA.setSelection({
+      anchor: {path: [{_key: 'A-4'}, 'children', {_key: 'A-3'}], offset: 3},
+      focus: {path: [{_key: 'A-4'}, 'children', {_key: 'A-3'}], offset: 3},
+    })
+    await editorA.pressKey('Backspace')
+
+    // And "undo" is performed
+    await editorA.undo()
+
+    // When "redo" is performed
+    await editorA.redo()
+
+    const valueAfterRedo = await editorA.getValue()
+    const blockAfterRedo =
+      valueAfterRedo && isPortableTextBlock(valueAfterRedo[0]) ? valueAfterRedo[0] : undefined
+
+    // Then the text is "foo"
+    expect(blockAfterRedo?.children[0]?.text).toBe('fo')
+
+    // And the text is marked with a "comment"
+    expect(blockAfterRedo?.children[0]?.marks).toEqual([commentKey])
+
+    // And the key is unchanged
+    expect(blockAfterRedo?.children[0]?._key).toBe(spanKey)
+  })
+
   it('Scenario: Undoing inserting text after annotated text', async () => {
     const [editorA] = await getEditors()
 
