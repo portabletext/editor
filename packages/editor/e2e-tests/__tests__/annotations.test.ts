b4648eb2a3382ea22cb048dd2fa4c7b69e1c402a
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-08-09 12:22:43 +0200

    test(editor): add failing test for undoing and redoing insertion after annotation

--- packages/editor/e2e-tests/__tests__/annotations.test.ts
+++ packages/editor/e2e-tests/__tests__/annotations.test.ts
@@ -201,4 +201,65 @@ describe('Feature: Annotations', () => {
     // And "bar" is unchanged
     expect(blockAfterUndo?.children[1]).toEqual(blockAfterEditorBMark?.children[1])
   })
+
+  it('Scenario: Undoing and redoing inserting text after annotated text', async () => {
+    const [editorA] = await getEditors()
+
+    // Given the text "foo"
+    await editorA.insertText('foo')
+
+    // And a "comment" around the text
+    await editorA.setSelection({
+      anchor: {path: [{_key: 'A-4'}, 'children', {_key: 'A-3'}], offset: 0},
+      focus: {path: [{_key: 'A-4'}, 'children', {_key: 'A-3'}], offset: 3},
+    })
+    await editorA.toggleMark('m')
+    const valueBeforeSpace = await editorA.getValue()
+    const commentKey =
+      valueBeforeSpace && isPortableTextBlock(valueBeforeSpace[0])
+        ? valueBeforeSpace[0].markDefs?.[0]?._key
+        : undefined
+
+    // When "Space" is pressed once
+    await editorA.setSelection({
+      anchor: {path: [{_key: 'A-4'}, 'children', {_key: 'A-3'}], offset: 3},
+      focus: {path: [{_key: 'A-4'}, 'children', {_key: 'A-3'}], offset: 3},
+    })
+    await editorA.pressKey(' ')
+
+    // And an "undo" is performed
+    await editorA.undo()
+
+    const valueAfterUndo = await editorA.getValue()
+    const blockAfterUndo =
+      valueAfterUndo && isPortableTextBlock(valueAfterUndo[0]) ? valueAfterUndo[0] : undefined
+
+    // Then the text is "foo"
+    expect(blockAfterUndo?.children[0]?.text).toBe('foo')
+
+    // And the text is marked with a "comment"
+    expect(blockAfterUndo?.children[0]?.marks).toEqual([commentKey])
+
+    // And the subsequent text is deleted
+    expect(blockAfterUndo?.children[1]).toBeUndefined()
+
+    // And when "redo" is performed
+    await editorA.redo()
+
+    const valueAfterRedo = await editorA.getValue()
+    const blockAfterRedo =
+      valueAfterRedo && isPortableTextBlock(valueAfterRedo[0]) ? valueAfterRedo[0] : undefined
+
+    // Then the text is "foo"
+    expect(blockAfterRedo?.children[0]?.text).toBe('foo')
+
+    // And the text is marked with a "comment"
+    expect(blockAfterRedo?.children[0]?.marks).toEqual([commentKey])
+
+    // And the subsequent text is " "
+    expect(blockAfterRedo?.children[1]?.text).toBe(' ')
+
+    // And the subsequent text has no marks
+    expect(blockAfterRedo?.children[1]?.marks).toEqual([])
+  })
 })
