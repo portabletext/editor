7abac2995a525c97f14353effdfbdb8ef49ee3b0
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-08-28 14:23:52 +0200

    test(editor): share Gherkin steps and port annotation scenarios to Gherkin

--- packages/editor/e2e-tests/__tests__/feature-block-objects.test.ts
+++ packages/editor/e2e-tests/__tests__/feature-block-objects.test.ts
@@ -1,27 +1,11 @@
 /** @jest-environment ./setup/collaborative.jest.env.ts */
-import {expect} from '@jest/globals'
 
 import featureFile from './block-objects.feature'
-import {defineStep, Feature} from './gherkin-driver'
-import {getEditorText, insertEditorText} from './step-helpers.test'
+import {Feature} from './gherkin-driver'
+import {parameterTypes, stepDefinitions} from './gherkin-step-definitions'
 
-Feature(featureFile, [
-  defineStep('the text {string}', async ({editorA}, text: string) => {
-    await insertEditorText(editorA, text)
-  }),
-  defineStep('an image', async ({editorA}) => {
-    await editorA.pressButton('insert-image')
-  }),
-  defineStep('{string} is typed', async ({editorA}, text: string) => {
-    await editorA.type(text)
-  }),
-  defineStep('{button} is pressed', async ({editorA}, button: string) => {
-    await editorA.pressKey(button)
-  }),
-  defineStep('editors have settled', async () => {
-    await waitForRevision()
-  }),
-  defineStep('the text is {text}', async ({editorA}, text: string) => {
-    await getEditorText(editorA).then((actualText) => expect(actualText).toEqual(text))
-  }),
-])
+Feature({
+  featureText: featureFile,
+  stepDefinitions,
+  parameterTypes,
+})
