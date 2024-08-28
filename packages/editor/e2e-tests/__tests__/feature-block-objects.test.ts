bdb8d282ab2968ff2cb155944ab48a78df9fdc2f
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-08-28 10:48:24 +0200

    test(editor): port block object scenarios to Gherkin

--- packages/editor/e2e-tests/__tests__/feature-block-objects.test.ts
+++ packages/editor/e2e-tests/__tests__/feature-block-objects.test.ts
@@ -1,173 +1,27 @@
 /** @jest-environment ./setup/collaborative.jest.env.ts */
-import {describe, expect, test} from '@jest/globals'
+import {expect} from '@jest/globals'
 
+import featureFile from './block-objects.feature'
+import {defineStep, Feature} from './gherkin-driver'
 import {getEditorText, insertEditorText} from './step-helpers.test'
 
-describe('Feature: Block Objects', () => {
-  test('Scenario: Pressing ArrowUp on a lonely image', async () => {
-    const [editorA] = await getEditors()
-
-    // Given an image
+Feature(featureFile, [
+  defineStep('the text {string}', async ({editorA}, text: string) => {
+    await insertEditorText(editorA, text)
+  }),
+  defineStep('an image', async ({editorA}) => {
     await editorA.pressButton('insert-image')
-
-    // When "ArrowUp" is pressed
-    await editorA.pressKey('ArrowUp')
+  }),
+  defineStep('{string} is typed', async ({editorA}, text: string) => {
+    await editorA.type(text)
+  }),
+  defineStep('{button} is pressed', async ({editorA}, button: string) => {
+    await editorA.pressKey(button)
+  }),
+  defineStep('editors have settled', async () => {
     await waitForRevision()
-
-    // Then the text is ",\n,image"
-    await getEditorText(editorA).then((text) => expect(text).toEqual(['', '\n', 'image']))
-  })
-
-  test('Scenario: Pressing ArrowDown on a lonely image', async () => {
-    const [editorA] = await getEditors()
-
-    // Given an image
-    await editorA.pressButton('insert-image')
-
-    // And "ArrowUp" is pressed
-    await editorA.pressKey('ArrowDown')
-    await waitForRevision()
-
-    // Then the text is "image,\n,"
-    await getEditorText(editorA).then((text) => expect(text).toEqual(['image', '\n', '']))
-  })
-
-  test('Scenario: Pressing ArrowDown on image at the bottom', async () => {
-    const [editorA] = await getEditors()
-
-    // Given the text "foo"
-    await insertEditorText(editorA, 'foo')
-
-    // And an image
-    await editorA.pressButton('insert-image')
-
-    // When "ArrowDown" is pressed
-    await editorA.pressKey('ArrowDown')
-    await waitForRevision()
-
-    // Then the text is ""
-    await getEditorText(editorA).then((text) =>
-      expect(text).toEqual(['foo', '\n', 'image', '\n', '']),
-    )
-  })
-
-  test('Scenario: Pressing Backspace on a lonely image', async () => {
-    const [editorA] = await getEditors()
-
-    // Given an image
-    await editorA.pressButton('insert-image')
-
-    // When "Backspace" is pressed
-    await editorA.pressKey('Backspace')
-
-    // And "foo" is typed
-    await editorA.type('foo')
-
-    // Then the text is "foo"
-    await getEditorText(editorA).then((text) => expect(text).toEqual(['foo']))
-  })
-
-  test('Scenario: Pressing Delete on a lonely image', async () => {
-    const [editorA] = await getEditors()
-
-    // Given an image
-    await editorA.pressButton('insert-image')
-
-    // When "Delete" is pressed
-    await editorA.pressKey('Delete')
-
-    // And "foo" is typed
-    await editorA.type('foo')
-
-    // Then the text is "foo"
-    await getEditorText(editorA).then((text) => expect(text).toEqual(['foo']))
-  })
-
-  test('Scenario: Pressing Backpace on image with text above', async () => {
-    const [editorA] = await getEditors()
-
-    // Given the text "foo"
-    await insertEditorText(editorA, 'foo')
-
-    // And an image
-    await editorA.pressButton('insert-image')
-
-    // When "Backspace" is pressed
-    await editorA.pressKey('Backspace')
-
-    // And "bar" is typed
-    await editorA.type('bar')
-
-    // Then the text is "foobar"
-    await getEditorText(editorA).then((text) => expect(text).toEqual(['foobar']))
-  })
-
-  test('Scenario: Pressing Delete on image with text above', async () => {
-    const [editorA] = await getEditors()
-
-    // Given the text "foo"
-    await insertEditorText(editorA, 'foo')
-
-    // And an image
-    await editorA.pressButton('insert-image')
-
-    // When "Delete" is pressed
-    await editorA.pressKey('Delete')
-
-    // And "bar" is typed
-    await editorA.type('bar')
-
-    // Then the text is "foobar"
-    await getEditorText(editorA).then((text) => expect(text).toEqual(['foobar']))
-  })
-
-  test('Scenario: Pressing Backpace on image with text below', async () => {
-    const [editorA] = await getEditors()
-
-    // Given an image
-    await editorA.pressButton('insert-image')
-
-    // When "Enter" is pressed
-    await editorA.pressKey('Enter')
-
-    // And "foo" is typed
-    await editorA.type('foo')
-
-    // And "ArrowUp" is pressed
-    await editorA.pressKey('ArrowUp')
-
-    // And "Backspace" is pressed
-    await editorA.pressKey('Backspace')
-
-    // And "bar" is typed
-    await editorA.type('bar')
-
-    // Then the text is "barfoo"
-    await getEditorText(editorA).then((text) => expect(text).toEqual(['barfoo']))
-  })
-
-  test('Scenario: Pressing Delete on image with text below', async () => {
-    const [editorA] = await getEditors()
-
-    // Given an image
-    await editorA.pressButton('insert-image')
-
-    // When "Enter" is pressed
-    await editorA.pressKey('Enter')
-
-    // And "foo" is typed
-    await editorA.type('foo')
-
-    // And "ArrowUp" is pressed
-    await editorA.pressKey('ArrowUp')
-
-    // And "Delete" is pressed
-    await editorA.pressKey('Delete')
-
-    // And "bar" is typed
-    await editorA.type('bar')
-
-    // Then the text is "barfoo"
-    await getEditorText(editorA).then((text) => expect(text).toEqual(['barfoo']))
-  })
-})
+  }),
+  defineStep('the text is {text}', async ({editorA}, text: string) => {
+    await getEditorText(editorA).then((actualText) => expect(actualText).toEqual(text))
+  }),
+])
