68b223660ed2eea27b95cb23c83db13094ad6b9e
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-08-28 09:48:27 +0200

    test(editor): port Jest config to TS and remove unused config

--- packages/editor/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const {createJestConfig} = require('./test-config/config.cjs')
-
-module.exports = createJestConfig({
-  displayName: require('./package.json').name,
-  modulePathIgnorePatterns: ['<rootDir>/e2e-tests'],
-})
