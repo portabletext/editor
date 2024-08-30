864aec07e1f52925bad2bc9ffa3fef509d1a5452
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-08-30 10:45:26 +0200

    test(editor): improve getTextMarks utility

--- packages/editor/e2e-tests/__tests__/gherkin-step-helpers.test.ts
+++ packages/editor/e2e-tests/__tests__/gherkin-step-helpers.test.ts
@@ -60,8 +60,21 @@ test(getTextMarks.name, () => {
       {_key: 's2', _type: 'span', text: 'r'},
     ],
   }
+  const splitFooBarBazBlock = {
+    _key: 'b1',
+    _type: 'block',
+    children: [
+      {_key: 's1', _type: 'span', text: 'foo '},
+      {_key: 's2', _type: 'span', text: 'bar', marks: ['strong']},
+      {_key: 's3', _type: 'span', text: ' '},
+      {_key: 's4', _type: 'span', text: 'baz', marks: ['l1']},
+    ],
+  }
 
   expect(getTextMarks([fooBlock, splitBarBlock], 'ba')).toEqual(['strong'])
+  expect(getTextMarks([splitFooBarBazBlock], 'bar')).toEqual(['strong'])
+  expect(getTextMarks([splitFooBarBazBlock], ' ')).toEqual([])
+  expect(getTextMarks([splitFooBarBazBlock], 'baz')).toEqual(['l1'])
 })
 
 test(getTextSelection.name, () => {
