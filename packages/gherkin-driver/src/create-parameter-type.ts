565f30f74225a587e71ad3642e8d213509342327
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-10-07 08:38:16 +0200

    test: add Vitest Gherkin driver

--- packages/gherkin-driver/src/create-parameter-type.ts
+++ packages/gherkin-driver/src/create-parameter-type.ts
@@ -1,5 +1,8 @@
 import {ParameterType} from '@cucumber/cucumber-expressions'
 
+/**
+ * @public
+ */
 export function createParameterType<T>(
   ...options: ConstructorParameters<typeof ParameterType<T>>
 ) {
--- packages/gherkin-driver/src/index.ts
+++ packages/gherkin-driver/src/index.ts
@@ -1,1 +1,2 @@
+export * from './step-definitions'
 export * from './create-parameter-type'
