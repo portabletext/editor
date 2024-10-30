43d0eca84b8dccdafc4d086972114fc6dccc4091
Cody Olsen <stipsan@gmail.com>
2024-10-30 17:35:15 +0100

    feat: use react-compiler to optimise render

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -3,5 +3,9 @@ import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {plugins: [['babel-plugin-react-compiler', {target: '18'}]]},
+    }),
+  ],
 })
