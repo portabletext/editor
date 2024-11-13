69985b4289d5179b4afc6ae32fe012f6f5927ff9
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-11-13 11:33:32 +0100

    fix: remove unneeded export

--- packages/editor/src/types/options.ts
+++ packages/editor/src/types/options.ts
@@ -1,14 +1,5 @@
 import type {BaseSyntheticEvent} from 'react'
-import type {EditorActor} from '../editor/editor-machine'
 import type {PortableTextEditor} from '../editor/PortableTextEditor'
-
-/**
- * @internal
- */
-export type createEditorOptions = {
-  editorActor: EditorActor
-  subscriptions: Array<() => () => void>
-}
 
 /**
  * @beta
