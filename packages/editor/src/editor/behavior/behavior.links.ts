5ca657eafc70ba7dd34ad57fd3ccd342c310f4ea
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-11-14 11:41:29 +0100

    feat(behavior): export small link behaviors plugin

---
+++ packages/editor/src/editor/behavior/behavior.links.ts
@@ -0,0 +1,91 @@
+import type {PortableTextMemberSchemaTypes} from '../../types/editor'
+import {defineBehavior} from './behavior.types'
+import {getFocusSpan, selectionIsCollapsed} from './behavior.utils'
+
+/**
+ * @alpha
+ */
+export type LinkBehaviorsConfig = {
+  mapLinkAnnotation?: (config: {
+    schema: PortableTextMemberSchemaTypes
+    url: string
+  }) => {name: string; value: {[prop: string]: unknown}} | undefined
+}
+
+/**
+ * @alpha
+ */
+export function createLinkBehaviors(config: LinkBehaviorsConfig) {
+  const pasteLinkOnSelection = defineBehavior({
+    on: 'paste',
+    guard: ({context, event}) => {
+      const selectionCollapsed = selectionIsCollapsed(context)
+      const text = event.clipboardData.getData('text/plain')
+      const url = looksLikeUrl(text) ? text : undefined
+      const annotation =
+        url !== undefined
+          ? config.mapLinkAnnotation?.({url, schema: context.schema})
+          : undefined
+
+      if (annotation && !selectionCollapsed) {
+        return {annotation}
+      }
+
+      return false
+    },
+    actions: [
+      (_, {annotation}) => [
+        {
+          type: 'annotation.add',
+          annotation,
+        },
+      ],
+    ],
+  })
+  const pasteLinkAtCaret = defineBehavior({
+    on: 'paste',
+    guard: ({context, event}) => {
+      const focusSpan = getFocusSpan(context)
+      const selectionCollapsed = selectionIsCollapsed(context)
+
+      if (!focusSpan || !selectionCollapsed) {
+        return false
+      }
+
+      const text = event.clipboardData.getData('text/plain')
+      const url = looksLikeUrl(text) ? text : undefined
+      const annotation =
+        url !== undefined
+          ? config.mapLinkAnnotation?.({url, schema: context.schema})
+          : undefined
+
+      if (url && annotation && selectionCollapsed) {
+        return {focusSpan, annotation, url}
+      }
+
+      return false
+    },
+    actions: [
+      (_, {annotation, url}) => [
+        {
+          type: 'insert span',
+          text: url,
+          annotations: [annotation],
+        },
+      ],
+    ],
+  })
+
+  const linkBehaviors = [pasteLinkOnSelection, pasteLinkAtCaret]
+
+  return linkBehaviors
+}
+
+function looksLikeUrl(text: string) {
+  let looksLikeUrl = false
+  try {
+    new URL(text)
+    looksLikeUrl = true
+  } catch {}
+  return looksLikeUrl
+}
