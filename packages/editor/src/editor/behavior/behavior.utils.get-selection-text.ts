c0eb8e0bb4672cc17ef57c9e06f5bed0ba6a4112
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-11-26 15:51:15 +0100

    fix(markdown behavior): allow inserting hr with trailing content

---
+++ packages/editor/src/editor/behavior/behavior.utils.get-selection-text.ts
@@ -0,0 +1,92 @@
+import {
+  isPortableTextSpan,
+  isPortableTextTextBlock,
+  type PortableTextBlock,
+} from '@sanity/types'
+import type {EditorSelection} from '../../types/editor'
+import {isKeyedSegment} from './behavior.utils.is-keyed-segment'
+import {reverseSelection} from './behavior.utils.reverse-selection'
+
+export function getSelectionText({
+  value,
+  selection,
+}: {
+  value: Array<PortableTextBlock>
+  selection: NonNullable<EditorSelection>
+}): string {
+  let text = ''
+
+  if (!value || !selection) {
+    return text
+  }
+
+  const forwardSelection = selection.backward
+    ? reverseSelection(selection)
+    : selection
+
+  if (!forwardSelection) {
+    return text
+  }
+
+  for (const block of value) {
+    if (
+      isKeyedSegment(forwardSelection.anchor.path[0]) &&
+      block._key !== forwardSelection.anchor.path[0]._key
+    ) {
+      continue
+    }
+
+    if (!isPortableTextTextBlock(block)) {
+      continue
+    }
+
+    for (const child of block.children) {
+      if (isPortableTextSpan(child)) {
+        if (
+          isKeyedSegment(forwardSelection.anchor.path[2]) &&
+          child._key === forwardSelection.anchor.path[2]._key &&
+          isKeyedSegment(forwardSelection.focus.path[2]) &&
+          child._key === forwardSelection.focus.path[2]._key
+        ) {
+          text =
+            text +
+            child.text.slice(
+              forwardSelection.anchor.offset,
+              forwardSelection.focus.offset,
+            )
+
+          break
+        }
+
+        if (
+          isKeyedSegment(forwardSelection.anchor.path[2]) &&
+          child._key === forwardSelection.anchor.path[2]._key
+        ) {
+          text = text + child.text.slice(forwardSelection.anchor.offset)
+          continue
+        }
+
+        if (
+          isKeyedSegment(forwardSelection.focus.path[2]) &&
+          child._key === forwardSelection.focus.path[2]._key
+        ) {
+          text = text + child.text.slice(0, forwardSelection.focus.offset)
+          break
+        }
+
+        if (text.length > 0) {
+          text + child.text
+        }
+      }
+    }
+
+    if (
+      isKeyedSegment(forwardSelection.focus.path[0]) &&
+      block._key === forwardSelection.focus.path[0]._key
+    ) {
+      break
+    }
+  }
+
+  return text
+}
