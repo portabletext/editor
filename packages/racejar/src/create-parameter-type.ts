50ceeed42c623bdc11a71259aa935577ccfe1563
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-11-26 09:35:12 +0100

    refactor(racejar): simplify `createParameterType`

--- packages/racejar/src/create-parameter-type.ts
+++ packages/racejar/src/create-parameter-type.ts
@@ -1,10 +1,27 @@
-import {ParameterType} from '@cucumber/cucumber-expressions'
+import {ParameterType, type RegExps} from '@cucumber/cucumber-expressions'
 
 /**
  * @public
  */
-export function createParameterType<T>(
-  ...options: ConstructorParameters<typeof ParameterType<T>>
-) {
-  return new ParameterType(...options)
+export type ParameterTypeConfig<TType = string> = {
+  readonly name: string
+  matcher: RegExps
+  type?: (...args: unknown[]) => TType
+  transform?: (...match: string[]) => TType
+}
+
+/**
+ * @public
+ */
+export function createParameterType<TType = string>(
+  config: ParameterTypeConfig<TType>,
+): ParameterType<TType> {
+  return new ParameterType(
+    config.name,
+    config.matcher,
+    config.type ?? String,
+    config.transform,
+    false,
+    true,
+  )
 }
