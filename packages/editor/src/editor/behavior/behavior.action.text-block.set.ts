391ab137dbec166a4092359b7e67ee4e467ae7f1
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-12-06 09:06:46 +0100

    fix(behavior): rename `(set|unset) block` to `text block.(set|unset)`

---
+++ packages/editor/src/editor/behavior/behavior.action.text-block.set.ts
@@ -0,0 +1,25 @@
+import {Transforms} from 'slate'
+import {toSlateRange} from '../../utils/ranges'
+import type {BehaviorActionImplementation} from './behavior.actions'
+
+export const textBlockSetActionImplementation: BehaviorActionImplementation<
+  'text block.set'
+> = ({action}) => {
+  const at = toSlateRange(
+    {
+      anchor: {path: action.at, offset: 0},
+      focus: {path: action.at, offset: 0},
+    },
+    action.editor,
+  )!
+
+  Transforms.setNodes(
+    action.editor,
+    {
+      ...(action.style ? {style: action.style} : {}),
+      ...(action.listItem ? {listItem: action.listItem} : {}),
+      ...(action.level ? {level: action.level} : {}),
+    },
+    {at},
+  )
+}
---
+++ packages/editor/src/editor/behavior/behavior.action.text-block.unset.ts
@@ -0,0 +1,17 @@
+import {Transforms} from 'slate'
+import {toSlateRange} from '../../utils/ranges'
+import type {BehaviorActionImplementation} from './behavior.actions'
+
+export const textBlockUnsetActionImplementation: BehaviorActionImplementation<
+  'text block.unset'
+> = ({action}) => {
+  const at = toSlateRange(
+    {
+      anchor: {path: action.at, offset: 0},
+      focus: {path: action.at, offset: 0},
+    },
+    action.editor,
+  )!
+
+  Transforms.unsetNodes(action.editor, action.props, {at})
+}
