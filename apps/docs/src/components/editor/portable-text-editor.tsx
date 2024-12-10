c0007ecf40b47c076992187cb3bbdccfaefbb4d8
Knut Melvær <knut@sanity.io>
2024-12-10 15:57:13 -0800

    wip: scaffold editor embed component

---
+++ apps/docs/src/components/editor/portable-text-editor.tsx
@@ -0,0 +1,14 @@
+/* 
+import {
+  EditorProvider,
+  Toolbar,
+  PortableTextEditable
+} from '@portabletext/editor' */
+
+export function PortableTextEditor() {
+  return (
+    <div>
+      <h1>Hi! I'm a Portable Text Editor!</h1>
+    </div>
+  )
+}
