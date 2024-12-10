762e7dd1a8fd7f4bf2b1297ba3c0a296ded9ca96
Knut Melvær <knut@sanity.io>
2024-12-10 15:56:55 -0800

    feat: add react component support

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,30 +1,31 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import react from '@astrojs/react'
+import starlight from '@astrojs/starlight'
+import {defineConfig} from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Portable Text Editor Docs',
-			social: {
-				github: 'https://github.com/portabletext/editor',
-			},
-			sidebar: [
-				{ slug: 'getting-started'},
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides'}
-				},
-				{
-					label: 'Concepts',
-					autogenerate: { directory: 'concepts'}
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-
-			],
-		}),
-	],
-});
+  integrations: [
+    starlight({
+      title: 'Portable Text Editor Docs',
+      social: {
+        github: 'https://github.com/portabletext/editor',
+      },
+      sidebar: [
+        {slug: 'getting-started'},
+        {
+          label: 'Guides',
+          autogenerate: {directory: 'guides'},
+        },
+        {
+          label: 'Concepts',
+          autogenerate: {directory: 'concepts'},
+        },
+        {
+          label: 'Reference',
+          autogenerate: {directory: 'reference'},
+        },
+      ],
+    }),
+    react(),
+  ],
+})
