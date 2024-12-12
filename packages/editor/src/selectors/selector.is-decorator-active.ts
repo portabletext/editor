4ce4e554de3c107689430c1fde08839beec9841d
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-12-12 08:16:02 +0100

    feat(selectors): add higher-order `isDecoratorActive`

---
+++ packages/editor/src/selectors/selector.is-decorator-active.ts
@@ -0,0 +1,16 @@
+import type {EditorSelector} from '../editor/editor-selector'
+import {getSelectedSpans} from './selector.get-selected-spans'
+
+/**
+ * @alpha
+ */
+export function isDecoratorActive(decorator: string): EditorSelector<boolean> {
+  return (snapshot) => {
+    const selectedSpans = getSelectedSpans(snapshot)
+
+    return (
+      selectedSpans.length > 0 &&
+      selectedSpans.every((span) => span.node.marks?.includes(decorator))
+    )
+  }
+}
