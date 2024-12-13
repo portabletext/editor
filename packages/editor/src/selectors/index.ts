37ba2567b7d299e7263cf4ce513e6184a28d7a14
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-12-13 08:16:58 +0100

    feat(selectors): add `getActiveStyle` and `isActiveStyle`

--- packages/editor/src/selectors/index.ts
+++ packages/editor/src/selectors/index.ts
@@ -8,8 +8,10 @@ export type {
 
 export type {EditorSchema} from '../editor/define-schema'
 export {getActiveListItem} from './selector.get-active-list-item'
+export {getActiveStyle} from './selector.get-active-style'
 export {getSelectedSpans} from './selector.get-selected-spans'
 export {getSelectionText} from './selector.get-selection-text'
 export {getBlockTextBefore} from './selector.get-text-before'
+export {isActiveStyle} from './selector.is-active-style'
 export {isDecoratorActive} from './selector.is-decorator-active'
 export * from './selectors'
---
+++ packages/editor/src/selectors/selector.get-active-style.ts
@@ -0,0 +1,37 @@
+import type {PortableTextTextBlock} from '@sanity/types'
+import {createGuards} from '../behavior-actions/behavior.guards'
+import type {EditorSelector} from '../editor/editor-selector'
+import {getSelectedBlocks} from './selectors'
+
+/**
+ * @alpha
+ */
+export const getActiveStyle: EditorSelector<PortableTextTextBlock['style']> = ({
+  context,
+}) => {
+  if (!context.selection) {
+    return undefined
+  }
+
+  const guards = createGuards(context)
+  const selectedBlocks = getSelectedBlocks({context}).map((block) => block.node)
+  const selectedTextBlocks = selectedBlocks.filter(guards.isTextBlock)
+
+  const firstTextBlock = selectedTextBlocks.at(0)
+
+  if (!firstTextBlock) {
+    return undefined
+  }
+
+  const firstStyle = firstTextBlock.style
+
+  if (!firstStyle) {
+    return undefined
+  }
+
+  if (selectedTextBlocks.every((block) => block.style === firstStyle)) {
+    return firstStyle
+  }
+
+  return undefined
+}
