6f924ea41d8eb455cae91bfe768d7fb76d64c8c6
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-12-19 11:48:59 +0100

    chore: add prettier-plugin-astro

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,5 +3,9 @@ import config from '@sanity/prettier-config'
 export default {
   ...config,
   printWidth: 80,
-  plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-gherkin'],
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-astro',
+    'prettier-plugin-gherkin',
+  ],
 }
