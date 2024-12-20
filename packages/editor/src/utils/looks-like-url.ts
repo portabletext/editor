a019665e0c5545e8e01c35ad7e8ca7128c97a547
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-12-20 07:55:46 +0100

    fix(link behavior): improve URL matching

--- packages/editor/src/utils/looks-like-url.ts
+++ packages/editor/src/utils/looks-like-url.ts
@@ -1,29 +1,13 @@
 export function looksLikeUrl(text: string) {
   let looksLikeUrl = false
   try {
-    try {
-      const url = new URL(text)
+    const url = new URL(text)
 
-      if (!sensibleProtocols.includes(url.protocol)) {
-        return false
-      }
-
-      const probablyHasTld =
-        url.hostname.length > 0 ? url.hostname.includes('.') : true
-
-      looksLikeUrl = probablyHasTld
-    } catch {
-      const url = new URL(`https://${text}`)
-
-      if (!sensibleProtocols.includes(url.protocol)) {
-        return false
-      }
-
-      const probablyHasTld =
-        url.hostname.length > 0 ? url.hostname.includes('.') : true
-
-      looksLikeUrl = probablyHasTld
+    if (!sensibleProtocols.includes(url.protocol)) {
+      return false
     }
+
+    looksLikeUrl = true
   } catch {}
   return looksLikeUrl
 }
