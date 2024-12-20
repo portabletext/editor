24c1a46cda95b9c5f8fb80431bebc10808444d2e
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2024-12-20 16:46:47 +0100

    fix: guard against trying to select a block directly

--- packages/editor/src/utils/paths.ts
+++ packages/editor/src/utils/paths.ts
@@ -49,16 +49,20 @@ export function toSlatePath(path: Path, editor: Editor): SlatePath {
         isKeySegment(path[0]) && (n as Descendant)._key === path[0]._key,
     }),
   )[0] || [undefined, undefined]
+
   if (!block || !Element.isElement(block)) {
     return []
   }
+
   if (editor.isVoid(block)) {
     return [blockPath[0], 0]
   }
+
   const childPath = [path[2]]
   const childIndex = block.children.findIndex((child) =>
     isEqual([{_key: child._key}], childPath),
   )
+
   if (childIndex >= 0 && block.children[childIndex]) {
     const child = block.children[childIndex]
     if (Element.isElement(child) && editor.isVoid(child)) {
@@ -66,5 +70,6 @@ export function toSlatePath(path: Path, editor: Editor): SlatePath {
     }
     return blockPath.concat(childIndex)
   }
-  return blockPath
+
+  return [blockPath[0], 0]
 }
