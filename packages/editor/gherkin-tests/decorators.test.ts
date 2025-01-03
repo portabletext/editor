1927a571582d3721f66905433300b524b8be763b
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-01-03 11:16:08 +0100

    test: add tests for overlapping decorators

--- packages/editor/gherkin-tests/decorators.test.ts
+++ packages/editor/gherkin-tests/decorators.test.ts
@@ -1,10 +1,17 @@
 import {Feature} from 'racejar/vitest'
+import decoratorsOverlappingFeature from '../gherkin-spec/decorators-overlapping.feature?raw'
 import decoratorsFeature from '../gherkin-spec/decorators.feature?raw'
 import {parameterTypes} from './gherkin-parameter-types'
 import {stepDefinitions} from './gherkin-step-definitions'
 
 Feature({
   featureText: decoratorsFeature,
+  stepDefinitions,
+  parameterTypes,
+})
+
+Feature({
+  featureText: decoratorsOverlappingFeature,
   stepDefinitions,
   parameterTypes,
 })
