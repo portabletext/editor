7f783d8b97a53eb693a4f96ffb8c3f897e5cb29e
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-01-07 12:59:31 +0100

    feat(selectors): add `isPointBeforeSelection`

---
+++ packages/editor/src/selectors/selector.is-point-before-selection.ts
@@ -0,0 +1,82 @@
+import {isKeySegment, isPortableTextTextBlock} from '@sanity/types'
+import type {EditorSelector} from '../editor/editor-selector'
+import {reverseSelection} from '../editor/utils/utils.reverse-selection'
+import type {EditorSelectionPoint} from '../types/editor'
+
+/**
+ * @public
+ */
+export function isPointBeforeSelection(
+  point: EditorSelectionPoint,
+): EditorSelector<boolean> {
+  return (snapshot) => {
+    if (!snapshot.context.selection) {
+      return false
+    }
+
+    const selection = reverseSelection(snapshot.context.selection)
+
+    const pointBlockKey = isKeySegment(point.path[0])
+      ? point.path[0]._key
+      : undefined
+    const pointChildKey = isKeySegment(point.path[2])
+      ? point.path[2]._key
+      : undefined
+
+    const startBlockKey = isKeySegment(selection.anchor.path[0])
+      ? selection.anchor.path[0]._key
+      : undefined
+    const startChildKey = isKeySegment(selection.anchor.path[2])
+      ? selection.anchor.path[2]._key
+      : undefined
+
+    if (!pointBlockKey || !startBlockKey) {
+      return false
+    }
+
+    let before = false
+
+    for (const block of snapshot.context.value) {
+      if (block._key === pointBlockKey) {
+        if (block._key !== startBlockKey) {
+          before = true
+          break
+        }
+
+        // Both the point and the selection start in this block
+
+        if (!isPortableTextTextBlock(block)) {
+          break
+        }
+
+        if (!pointChildKey || !startChildKey) {
+          break
+        }
+
+        for (const child of block.children) {
+          if (child._key === pointChildKey) {
+            if (child._key !== startChildKey) {
+              before = true
+              break
+            }
+
+            // Both the point and the selection start in this child
+
+            before = point.offset < selection.anchor.offset
+            break
+          }
+
+          if (child._key === startChildKey) {
+            break
+          }
+        }
+      }
+
+      if (block._key === startBlockKey) {
+        break
+      }
+    }
+
+    return before
+  }
+}
