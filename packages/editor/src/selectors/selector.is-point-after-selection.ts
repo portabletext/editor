83f50f79e4330c89db9ca280828bf41dca697677
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-01-08 08:53:13 +0100

    feat(selectors): add `isPointAfterSelection`

---
+++ packages/editor/src/selectors/selector.is-point-after-selection.ts
@@ -0,0 +1,82 @@
+import {isKeySegment, isPortableTextTextBlock} from '@sanity/types'
+import type {EditorSelector} from '../editor/editor-selector'
+import {reverseSelection} from '../editor/utils/utils.reverse-selection'
+import type {EditorSelectionPoint} from '../types/editor'
+
+/**
+ * @public
+ */
+export function isPointAfterSelection(
+  point: EditorSelectionPoint,
+): EditorSelector<boolean> {
+  return (snapshot) => {
+    if (!snapshot.context.selection) {
+      return false
+    }
+
+    const selection = reverseSelection(snapshot.context.selection)
+
+    const pointBlockKey = isKeySegment(point.path[0])
+      ? point.path[0]._key
+      : undefined
+    const pointChildKey = isKeySegment(point.path[2])
+      ? point.path[2]._key
+      : undefined
+
+    const endBlockKey = isKeySegment(selection.focus.path[0])
+      ? selection.focus.path[0]._key
+      : undefined
+    const endChildKey = isKeySegment(selection.focus.path[2])
+      ? selection.focus.path[2]._key
+      : undefined
+
+    if (!pointBlockKey || !endBlockKey) {
+      return false
+    }
+
+    let after = false
+
+    for (const block of snapshot.context.value) {
+      if (block._key === endBlockKey) {
+        if (block._key !== pointBlockKey) {
+          after = true
+          break
+        }
+
+        // Both the point and the selection end in this block
+
+        if (!isPortableTextTextBlock(block)) {
+          break
+        }
+
+        if (!pointChildKey || !endChildKey) {
+          break
+        }
+
+        for (const child of block.children) {
+          if (child._key === endChildKey) {
+            if (child._key !== pointChildKey) {
+              after = true
+              break
+            }
+
+            // Both the point and the selection end in this child
+
+            after = point.offset > selection.focus.offset
+            break
+          }
+
+          if (child._key === pointChildKey) {
+            break
+          }
+        }
+      }
+
+      if (block._key === pointBlockKey) {
+        break
+      }
+    }
+
+    return after
+  }
+}
