e1d03e828bdb9fab523a71af65bd8deefef8cae2
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-01-16 10:22:25 +0100

    refactor: abstract `createEditorSnapshot`

--- packages/editor/src/editor/editor-snapshot.ts
+++ packages/editor/src/editor/editor-snapshot.ts
@@ -1,6 +1,10 @@
 import type {PortableTextBlock} from '@sanity/types'
-import type {EditorSelection} from '../types/editor'
+import {toPortableTextRange} from '../internal-utils/ranges'
+import {fromSlateValue} from '../internal-utils/values'
+import {KEY_TO_VALUE_ELEMENT} from '../internal-utils/weakMaps'
+import type {EditorSelection, PortableTextSlateEditor} from '../types/editor'
 import type {EditorSchema} from './define-schema'
+import {getActiveDecorators} from './get-active-decorators'
 
 /**
  * @public
@@ -18,4 +22,36 @@ export type EditorContext = {
  */
 export type EditorSnapshot = {
   context: EditorContext
+}
+
+export function createEditorSnapshot({
+  editor,
+  keyGenerator,
+  schema,
+}: {
+  editor: PortableTextSlateEditor
+  keyGenerator: () => string
+  schema: EditorSchema
+}) {
+  const value = fromSlateValue(
+    editor.children,
+    schema.block.name,
+    KEY_TO_VALUE_ELEMENT.get(editor),
+  )
+  const selection = toPortableTextRange(value, editor.selection, schema)
+
+  const context = {
+    activeDecorators: getActiveDecorators({
+      schema,
+      slateEditorInstance: editor,
+    }),
+    keyGenerator,
+    schema,
+    selection,
+    value,
+  } satisfies EditorContext
+
+  return {
+    context,
+  } satisfies EditorSnapshot
 }
