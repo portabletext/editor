8383b3bf14106c889d75c86a34311e699e38312e
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-01-22 15:13:36 +0100

    feat(behaviors): promote `annotation.toggle` to an event

--- packages/editor/src/editor/editor-machine.ts
+++ packages/editor/src/editor/editor-machine.ts
@@ -185,6 +185,7 @@ export type InternalEditorEmittedEvent =
       'type',
       | 'annotation.add'
       | 'annotation.remove'
+      | 'annotation.toggle'
       | 'blur'
       | 'decorator.add'
       | 'decorator.remove'
