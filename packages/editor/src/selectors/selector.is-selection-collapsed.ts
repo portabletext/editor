ef6b9464fd983d97e0dfbc3bd7dfb71fd3c80845
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-01-23 11:02:58 +0100

    fix(selectors): account for no selection in `isSelectionCollapsed`

--- packages/editor/src/selectors/selector.is-selection-collapsed.ts
+++ packages/editor/src/selectors/selector.is-selection-collapsed.ts
@@ -4,9 +4,13 @@ import type {EditorSelector} from '../editor/editor-selector'
  * @public
  */
 export const isSelectionCollapsed: EditorSelector<boolean> = ({context}) => {
+  if (!context.selection) {
+    return false
+  }
+
   return (
-    JSON.stringify(context.selection?.anchor.path) ===
-      JSON.stringify(context.selection?.focus.path) &&
+    JSON.stringify(context.selection.anchor.path) ===
+      JSON.stringify(context.selection.focus.path) &&
     context.selection?.anchor.offset === context.selection?.focus.offset
   )
 }
