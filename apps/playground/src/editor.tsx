9b917da7f1332380b23abf3bb589fd96ccbaaa68
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-01-29 11:48:49 +0100

    refactor(playground): use `MarkdownPlugin`

--- apps/playground/src/editor.tsx
+++ apps/playground/src/editor.tsx
@@ -18,8 +18,8 @@ import {
   createCodeEditorBehaviors,
   createEmojiPickerBehaviors,
   createLinkBehaviors,
-  createMarkdownBehaviors,
 } from '@portabletext/editor/behaviors'
+import {MarkdownPlugin} from '@portabletext/editor/plugins'
 import {useSelector} from '@xstate/react'
 import {CopyIcon, ImageIcon, TrashIcon} from 'lucide-react'
 import {useEffect, useState, type JSX} from 'react'
@@ -353,47 +353,32 @@ function EditorPlaygroundToolbar(props: {
           <ValuePreview editorId={props.editorRef.id} />
         ) : null}
       </div>
-      {enableMarkdownPlugin ? <MarkdownPlugin /> : null}
+      {enableMarkdownPlugin ? (
+        <MarkdownPlugin
+          config={{
+            horizontalRuleObject: ({schema}) => {
+              const name = schema.blockObjects.find(
+                (object) => object.name === 'break',
+              )?.name
+              return name ? {name} : undefined
+            },
+            defaultStyle: ({schema}) => schema.styles[0].value,
+            headingStyle: ({schema, level}) =>
+              schema.styles.find((style) => style.value === `h${level}`)?.value,
+            blockquoteStyle: ({schema}) =>
+              schema.styles.find((style) => style.value === 'blockquote')
+                ?.value,
+            unorderedListStyle: ({schema}) =>
+              schema.lists.find((list) => list.value === 'bullet')?.value,
+            orderedListStyle: ({schema}) =>
+              schema.lists.find((list) => list.value === 'number')?.value,
+          }}
+        />
+      ) : null}
       {enableCodeEditorPlugin ? <CodeEditorPlugin /> : null}
       {enableLinkPlugin ? <LinkPlugin /> : null}
     </>
   )
-}
-
-function MarkdownPlugin() {
-  const editor = useEditor()
-
-  useEffect(() => {
-    const behaviors = createMarkdownBehaviors({
-      horizontalRuleObject: ({schema}) => {
-        const name = schema.blockObjects.find(
-          (object) => object.name === 'break',
-        )?.name
-        return name ? {name} : undefined
-      },
-      defaultStyle: ({schema}) => schema.styles[0].value,
-      headingStyle: ({schema, level}) =>
-        schema.styles.find((style) => style.value === `h${level}`)?.value,
-      blockquoteStyle: ({schema}) =>
-        schema.styles.find((style) => style.value === 'blockquote')?.value,
-      unorderedListStyle: ({schema}) =>
-        schema.lists.find((list) => list.value === 'bullet')?.value,
-      orderedListStyle: ({schema}) =>
-        schema.lists.find((list) => list.value === 'number')?.value,
-    })
-
-    const unregisterBehaviors = behaviors.map((behavior) =>
-      editor.registerBehavior({behavior}),
-    )
-
-    return () => {
-      for (const unregisterBehavior of unregisterBehaviors) {
-        unregisterBehavior()
-      }
-    }
-  }, [editor])
-
-  return null
 }
 
 function EmojiPickerPlugin() {
