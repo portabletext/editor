0a820970bebb4112674debee14b74948eb9f35a7
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-01-29 09:54:55 +0100

    refactor: abstract `EditorRefPlugin`

---
+++ packages/editor/src/react-plugins/editor-ref-plugin.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import type {Editor} from '../editor/create-editor'
+import {useEditor} from '../editor/editor-provider'
+
+export const EditorRefPlugin = React.forwardRef<Editor | null>((_, ref) => {
+  const editor = useEditor()
+
+  const portableTextEditorRef = React.useRef(editor)
+
+  React.useImperativeHandle(ref, () => portableTextEditorRef.current, [])
+
+  return null
+})
+EditorRefPlugin.displayName = 'EditorRefPlugin'
