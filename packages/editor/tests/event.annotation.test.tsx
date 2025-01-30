f4328a653d5852cfd49205c6d3c1a2900b0b7ded
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-01-30 08:06:48 +0100

    test: add `annotation.(add|remove)` test

---
+++ packages/editor/tests/event.annotation.test.tsx
@@ -0,0 +1,114 @@
+import {page, userEvent} from '@vitest/browser/context'
+import React from 'react'
+import {describe, expect, test, vi} from 'vitest'
+import {render} from 'vitest-browser-react'
+import {
+  getSelectionBeforeText,
+  getTextMarks,
+  getTextSelection,
+  getValueText,
+} from '../gherkin-tests/gherkin-step-helpers'
+import {
+  defineSchema,
+  EditorProvider,
+  PortableTextEditable,
+  type Editor,
+} from '../src'
+import {createTestKeyGenerator} from '../src/internal-utils/test-key-generator'
+import {EditorRefPlugin} from '../src/plugins'
+
+describe('event.annotation', () => {
+  test('.add/.remove', async () => {
+    const editorRef = React.createRef<Editor>()
+
+    render(
+      <EditorProvider
+        initialConfig={{
+          keyGenerator: createTestKeyGenerator(),
+          schemaDefinition: defineSchema({
+            decorators: [{name: 'strong'}],
+            annotations: [{name: 'link'}],
+          }),
+        }}
+      >
+        <EditorRefPlugin ref={editorRef} />
+        <PortableTextEditable />
+      </EditorProvider>,
+    )
+
+    const locator = page.getByRole('textbox')
+
+    await vi.waitFor(() => expect.element(locator).toBeInTheDocument())
+
+    await userEvent.click(locator)
+    await userEvent.fill(locator, 'Hello, world!')
+
+    editorRef.current?.send({
+      type: 'select',
+      selection: getTextSelection(
+        editorRef.current?.getSnapshot().context.value,
+        'world',
+      ),
+    })
+
+    editorRef.current?.send({
+      type: 'annotation.add',
+      annotation: {
+        name: 'link',
+        value: {
+          href: 'https://sanity.io',
+        },
+      },
+    })
+
+    editorRef.current?.send({
+      type: 'select',
+      selection: getTextSelection(
+        editorRef.current?.getSnapshot().context.value,
+        'Hello',
+      ),
+    })
+
+    editorRef.current?.send({
+      type: 'annotation.add',
+      annotation: {
+        name: 'link',
+        value: {
+          href: 'https://portabletext.org',
+        },
+      },
+    })
+
+    expect(
+      getValueText(editorRef.current?.getSnapshot().context.value),
+    ).toEqual(['Hello', ', ', 'world', '!'])
+    expect(
+      getTextMarks(editorRef.current?.getSnapshot().context.value, 'Hello'),
+    ).toEqual(['k5'])
+    expect(
+      getTextMarks(editorRef.current?.getSnapshot().context.value, 'world'),
+    ).toEqual(['k2'])
+
+    editorRef.current?.send({
+      type: 'select',
+      selection: getSelectionBeforeText(
+        editorRef.current?.getSnapshot().context.value,
+        'ld',
+      ),
+    })
+
+    editorRef.current?.send({
+      type: 'annotation.remove',
+      annotation: {
+        name: 'link',
+      },
+    })
+
+    expect(
+      getValueText(editorRef.current?.getSnapshot().context.value),
+    ).toEqual(['Hello', ', world!'])
+    expect(
+      getTextMarks(editorRef.current?.getSnapshot().context.value, 'Hello'),
+    ).toEqual(['k5'])
+  })
+})
