36b0a36c26329404819ebdaea15dc4edf34c9bb4
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-02-07 13:16:32 +0100

    refactor: simplify `addAnnotationActionImplementation`

--- packages/editor/src/editor/plugins/createWithEditableAPI.ts
+++ packages/editor/src/editor/plugins/createWithEditableAPI.ts
@@ -629,118 +629,110 @@ export const addAnnotationActionImplementation: BehaviorActionImplementation<
   AddedAnnotationPaths | undefined
 > = ({context, action}) => {
   const editor = action.editor
-  const {selection: originalSelection} = editor
-  let paths: AddedAnnotationPaths | undefined = undefined
 
-  if (originalSelection) {
-    if (Range.isCollapsed(originalSelection)) {
-      return
+  if (!editor.selection || Range.isCollapsed(editor.selection)) {
+    return
+  }
+
+  let paths: AddedAnnotationPaths | undefined = undefined
+  let spanPath: Path | undefined
+  let markDefPath: Path | undefined
+  const markDefPaths: Path[] = []
+
+  const selectedBlocks = Editor.nodes(editor, {
+    at: editor.selection,
+    match: (node) => editor.isTextBlock(node),
+    reverse: Range.isBackward(editor.selection),
+  })
+
+  for (const [block, blockPath] of selectedBlocks) {
+    if (block.children.length === 0) {
+      continue
     }
 
-    // If we still have a selection, add the annotation to the selected text
-    if (editor.selection) {
-      let spanPath: Path | undefined
-      let markDefPath: Path | undefined
-      const markDefPaths: Path[] = []
+    if (block.children.length === 1 && block.children[0].text === '') {
+      continue
+    }
 
-      if (!editor.selection) {
-        return
+    const annotationKey = context.keyGenerator()
+    const markDefs = block.markDefs ?? []
+    const existingMarkDef = markDefs.find(
+      (markDef) =>
+        markDef._type === action.annotation.name &&
+        markDef._key === annotationKey,
+    )
+
+    if (existingMarkDef === undefined) {
+      Transforms.setNodes(
+        editor,
+        {
+          markDefs: [
+            ...markDefs,
+            {
+              _type: action.annotation.name,
+              _key: annotationKey,
+              ...action.annotation.value,
+            },
+          ],
+        },
+        {at: blockPath},
+      )
+
+      markDefPath = [{_key: block._key}, 'markDefs', {_key: annotationKey}]
+
+      if (Range.isBackward(editor.selection)) {
+        markDefPaths.unshift(markDefPath)
+      } else {
+        markDefPaths.push(markDefPath)
+      }
+    }
+
+    Transforms.setNodes(editor, {}, {match: Text.isText, split: true})
+
+    const children = Node.children(editor, blockPath)
+
+    for (const [span, path] of children) {
+      if (!editor.isTextSpan(span)) {
+        continue
       }
 
-      const selectedBlocks = Editor.nodes(editor, {
-        at: editor.selection,
-        match: (node) => editor.isTextBlock(node),
-        reverse: Range.isBackward(editor.selection),
-      })
+      if (!Range.includes(editor.selection, path)) {
+        continue
+      }
 
-      for (const [block, blockPath] of selectedBlocks) {
-        if (block.children.length === 0) {
-          continue
-        }
-
-        if (block.children.length === 1 && block.children[0].text === '') {
-          continue
-        }
-
-        const annotationKey = context.keyGenerator()
-        const markDefs = block.markDefs ?? []
-        const existingMarkDef = markDefs.find(
+      const marks = span.marks ?? []
+      const existingSameTypeAnnotations = marks.filter((mark) =>
+        markDefs.some(
           (markDef) =>
-            markDef._type === action.annotation.name &&
-            markDef._key === annotationKey,
-        )
+            markDef._key === mark && markDef._type === action.annotation.name,
+        ),
+      )
 
-        if (existingMarkDef === undefined) {
-          Transforms.setNodes(
-            editor,
-            {
-              markDefs: [
-                ...markDefs,
-                {
-                  _type: action.annotation.name,
-                  _key: annotationKey,
-                  ...action.annotation.value,
-                },
-              ],
-            },
-            {at: blockPath},
-          )
-
-          markDefPath = [{_key: block._key}, 'markDefs', {_key: annotationKey}]
-          if (Range.isBackward(editor.selection)) {
-            markDefPaths.unshift(markDefPath)
-          } else {
-            markDefPaths.push(markDefPath)
-          }
-        }
-
-        Transforms.setNodes(editor, {}, {match: Text.isText, split: true})
-
-        const children = Node.children(editor, blockPath)
-
-        for (const [span, path] of children) {
-          if (!editor.isTextSpan(span)) {
-            continue
-          }
-
-          if (!Range.includes(editor.selection, path)) {
-            continue
-          }
-
-          const marks = span.marks ?? []
-          const existingSameTypeAnnotations = marks.filter((mark) =>
-            markDefs.some(
-              (markDef) =>
-                markDef._key === mark &&
-                markDef._type === action.annotation.name,
+      Transforms.setNodes(
+        editor,
+        {
+          marks: [
+            ...marks.filter(
+              (mark) => !existingSameTypeAnnotations.includes(mark),
             ),
-          )
+            annotationKey,
+          ],
+        },
+        {at: path},
+      )
 
-          Transforms.setNodes(
-            editor,
-            {
-              marks: [
-                ...marks.filter(
-                  (mark) => !existingSameTypeAnnotations.includes(mark),
-                ),
-                annotationKey,
-              ],
-            },
-            {at: path},
-          )
-          spanPath = [{_key: block._key}, 'children', {_key: span._key}]
-        }
-      }
-
-      if (markDefPath && spanPath) {
-        paths = {
-          markDefPath,
-          markDefPaths,
-          spanPath,
-        }
-      }
+      spanPath = [{_key: block._key}, 'children', {_key: span._key}]
     }
   }
+
+  if (markDefPath && spanPath) {
+    paths = {
+      markDefPath,
+      markDefPaths,
+      spanPath,
+    }
+  }
+
   return paths
 }
 
