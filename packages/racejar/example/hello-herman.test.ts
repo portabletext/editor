9a80cd34237e60bf9ca873870d073b5af70eb3e8
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2025-02-08 18:57:41 +0000

    Add support for Tables and DocStrings

--- packages/racejar/example/hello-herman.test.ts
+++ packages/racejar/example/hello-herman.test.ts
@@ -2,8 +2,8 @@ import {expect} from 'vitest'
 import {Given, Then, When} from '../src/step-definitions'
 import {Feature} from '../src/vitest'
 
-function greet(name: string) {
-  return `Hello, ${name}!`
+function greet(name: string, greeting: string) {
+  return `Hello ${name}, ${greeting}`
 }
 
 type Context = {
@@ -16,15 +16,19 @@ Feature({
     Feature: Greeting
       Scenario: Greeting a person
         Given the person "Herman"
-        When greeting the person
-        Then the greeting is "Hello, Herman!"`,
+        When greeting the person with:
+          | how are you? |
+        Then the greeting is "Hello Herman, how are you?"`,
   stepDefinitions: [
     Given('the person {string}', (context: Context, person: string) => {
       context.person = person
     }),
-    When('greeting the person', (context: Context) => {
-      context.greeting = greet(context.person)
-    }),
+    When(
+      'greeting the person with:',
+      (context: Context, greeting: string[][]) => {
+        context.greeting = greet(context.person, greeting[0][0])
+      },
+    ),
     Then('the greeting is {string}', (context: Context, greeting: string) => {
       expect(context.greeting).toBe(greeting)
     }),
