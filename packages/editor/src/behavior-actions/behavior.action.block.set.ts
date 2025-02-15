b578d70d262349f689015729a60c79d805e2afee
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-02-15 16:44:32 +0100

    fix(`block.set`): remove reliance on knowing block type
    Before this change, you'd have to know the type of the block and if the type
    was non-text, you'd have to know to set the `value` prop. The type signature
    for the event object was flat-out wrong. This has now been fixed by requering
    any new props to be added to a `props` prop. Under the hood we parse the
    resulting block to make sure it's still valid, generate a new `_key` if needed
    and guard against updating its `_type`.

--- packages/editor/src/behavior-actions/behavior.action.block.set.ts
+++ packages/editor/src/behavior-actions/behavior.action.block.set.ts
@@ -1,10 +1,13 @@
-import {Transforms} from 'slate'
+import {Editor, Transforms, type Element as SlateElement} from 'slate'
+import {parseBlock} from '../internal-utils/parse-blocks'
 import {toSlateRange} from '../internal-utils/ranges'
+import {fromSlateValue, toSlateValue} from '../internal-utils/values'
+import {KEY_TO_VALUE_ELEMENT} from '../internal-utils/weakMaps'
 import type {BehaviorActionImplementation} from './behavior.actions'
 
 export const blockSetBehaviorActionImplementation: BehaviorActionImplementation<
   'block.set'
-> = ({action}) => {
+> = ({context, action}) => {
   const location = toSlateRange(
     {
       anchor: {path: action.at, offset: 0},
@@ -14,10 +17,50 @@ export const blockSetBehaviorActionImplementation: BehaviorActionImplementation<
   )
 
   if (!location) {
-    return
+    throw new Error(
+      `Unable to convert ${JSON.stringify(action.at)} into a Slate Range`,
+    )
   }
 
-  const {at, editor, type, ...payload} = action
+  const blockEntry = Editor.node(action.editor, location, {depth: 1})
+  const block = blockEntry?.[0]
 
-  Transforms.setNodes(action.editor, payload, {at: location})
+  if (!block) {
+    throw new Error(`Unable to find block at ${JSON.stringify(action.at)}`)
+  }
+
+  const parsedBlock = fromSlateValue(
+    [block],
+    context.schema.block.name,
+    KEY_TO_VALUE_ELEMENT.get(action.editor),
+  ).at(0)
+
+  if (!parsedBlock) {
+    throw new Error(`Unable to parse block at ${JSON.stringify(action.at)}`)
+  }
+
+  const {_type, ...filteredProps} = action.props
+
+  const updatedBlock = parseBlock({
+    context,
+    block: {
+      ...parsedBlock,
+      ...filteredProps,
+    },
+    options: {refreshKeys: false},
+  })
+
+  if (!updatedBlock) {
+    throw new Error(`Unable to update block at ${JSON.stringify(action.at)}`)
+  }
+
+  const slateBlock = toSlateValue([updatedBlock], {
+    schemaTypes: context.schema,
+  })?.at(0) as SlateElement | undefined
+
+  if (!slateBlock) {
+    throw new Error(`Unable to convert block to Slate value`)
+  }
+
+  Transforms.setNodes(action.editor, slateBlock, {at: location})
 }
