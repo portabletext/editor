a30afea94535c12caf050f21e360e91c33b48726
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-02-18 05:24:19 +0100

    feat(utils): add `childSelectionPointToBlockOffset`

--- packages/editor/src/utils/index.ts
+++ packages/editor/src/utils/index.ts
@@ -5,6 +5,7 @@ export {
   spanSelectionPointToBlockOffset,
 } from './util.block-offset'
 export {blockOffsetsToSelection} from './util.block-offsets-to-selection'
+export {childSelectionPointToBlockOffset} from './util.child-selection-point-to-block-offset'
 export {getBlockEndPoint} from './util.get-block-end-point'
 export {getBlockStartPoint} from './util.get-block-start-point'
 export {getTextBlockText} from './util.get-text-block-text'
