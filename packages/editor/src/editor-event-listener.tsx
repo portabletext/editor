148a02216d8b9699e3223f16db5ba29b8b216bc5
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-02-21 09:03:27 +0100

    fix: deprecate `EditorEventListener`

---
+++ packages/editor/src/editor-event-listener.tsx
@@ -0,0 +1,30 @@
+import {useEffect} from 'react'
+import {useEffectEvent} from 'use-effect-event'
+import type {EditorEmittedEvent} from './editor/editor-machine'
+import {useEditor} from './editor/editor-provider'
+
+/**
+ * @public
+ * @deprecated
+ * This component has been renamed. Use `EventListenerPlugin` instead.
+ *
+ * ```
+ * import {EventListenerPlugin} from '@portabletext/editor/plugins'
+ * ```
+ */
+export function EditorEventListener(props: {
+  on: (event: EditorEmittedEvent) => void
+}) {
+  const editor = useEditor()
+  const on = useEffectEvent(props.on)
+
+  useEffect(() => {
+    const subscription = editor.on('*', on)
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [editor])
+
+  return null
+}
