c2848517882b221a381aae8662b2328e68b62ac8
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-03-07 11:54:55 +0100

    refactor: use `insert.block` in `insert.block object` and `insert.text block`

--- packages/editor/src/behavior-actions/behavior.action.insert.block-object.ts
+++ packages/editor/src/behavior-actions/behavior.action.insert.block-object.ts
@@ -1,25 +1,20 @@
-import {toSlateValue} from '../internal-utils/values'
-import {insertBlock} from './behavior.action-utils.insert-block'
+import {insertBlockActionImplementation} from './behavior.action.insert.block'
 import type {BehaviorActionImplementation} from './behavior.actions'
 
 export const insertBlockObjectActionImplementation: BehaviorActionImplementation<
   'insert.block object'
 > = ({context, action}) => {
-  const block = toSlateValue(
-    [
-      {
+  insertBlockActionImplementation({
+    context,
+    action: {
+      type: 'insert.block',
+      block: {
         _key: context.keyGenerator(),
         _type: action.blockObject.name,
         ...(action.blockObject.value ? action.blockObject.value : {}),
       },
-    ],
-    {schemaTypes: context.schema},
-  )[0]
-
-  insertBlock({
-    block,
-    placement: action.placement,
-    editor: action.editor,
-    schema: context.schema,
+      editor: action.editor,
+      placement: action.placement,
+    },
   })
 }
--- packages/editor/src/behavior-actions/behavior.action.insert.block.ts
+++ packages/editor/src/behavior-actions/behavior.action.insert.block.ts
@@ -1,6 +1,8 @@
+import {Editor, Transforms, type Descendant} from 'slate'
+import type {EditorSchema} from '../editor/define-schema'
 import {parseBlock} from '../internal-utils/parse-blocks'
-import {toSlateValue} from '../internal-utils/values'
-import {insertBlock} from './behavior.action-utils.insert-block'
+import {isEqualToEmptyEditor, toSlateValue} from '../internal-utils/values'
+import type {PortableTextSlateEditor} from '../types/editor'
 import type {BehaviorActionImplementation} from './behavior.actions'
 
 export const insertBlockActionImplementation: BehaviorActionImplementation<
@@ -30,4 +32,61 @@ export const insertBlockActionImplementation: BehaviorActionImplementation<
     editor: action.editor,
     schema: context.schema,
   })
+}
+
+function insertBlock({
+  block,
+  placement,
+  editor,
+  schema,
+}: {
+  block: Descendant
+  placement: 'auto' | 'after' | 'before'
+  editor: PortableTextSlateEditor
+  schema: EditorSchema
+}) {
+  if (!editor.selection) {
+    const lastBlock = Array.from(
+      Editor.nodes(editor, {
+        match: (n) => !Editor.isEditor(n),
+        at: [],
+        reverse: true,
+      }),
+    )[0]
+
+    // If there is no selection, let's just insert the new block at the
+    // end of the document
+    Editor.insertNode(editor, block)
+
+    if (lastBlock && isEqualToEmptyEditor([lastBlock[0]], schema)) {
+      // And if the last block was an empty text block, let's remove
+      // that too
+      Transforms.removeNodes(editor, {at: lastBlock[1]})
+    }
+  } else {
+    const [focusBlock, focusBlockPath] = Array.from(
+      Editor.nodes(editor, {
+        at: editor.selection.focus.path.slice(0, 1),
+        match: (n) => !Editor.isEditor(n),
+      }),
+    )[0] ?? [undefined, undefined]
+
+    if (placement === 'after') {
+      const nextPath = [focusBlockPath[0] + 1]
+
+      Transforms.insertNodes(editor, block, {at: nextPath})
+      Transforms.select(editor, {
+        anchor: {path: [nextPath[0], 0], offset: 0},
+        focus: {path: [nextPath[0], 0], offset: 0},
+      })
+    } else if (placement === 'before') {
+      Transforms.insertNodes(editor, block, {at: focusBlockPath})
+    } else {
+      Editor.insertNode(editor, block)
+
+      if (focusBlock && isEqualToEmptyEditor([focusBlock], schema)) {
+        Transforms.removeNodes(editor, {at: focusBlockPath})
+      }
+    }
+  }
 }
