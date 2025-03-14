dfabff01504cd480870a4be46c91ef2d189d04cd
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-03-14 08:10:03 +0100

    chore: add internal `getDragSelection` util

---
+++ packages/editor/src/internal-utils/drag-selection.test.ts
@@ -0,0 +1,249 @@
+import {describe, expect, test} from 'vitest'
+import {compileSchemaDefinition, defineSchema} from '../editor/define-schema'
+import type {EditorSelection} from '../types/editor'
+import {createTestSnapshot} from './create-test-snapshot'
+import {getDragSelection} from './drag-selection'
+import {createTestKeyGenerator} from './test-key-generator'
+
+describe(getDragSelection.name, () => {
+  const keyGenerator = createTestKeyGenerator()
+  const foo = {
+    _key: keyGenerator(),
+    _type: 'block',
+    children: [
+      {
+        _key: keyGenerator(),
+        _type: 'span',
+        text: 'foo',
+      },
+    ],
+  }
+  const fooPath = [{_key: foo._key}, 'children', {_key: foo.children[0]._key}]
+  const bar = {
+    _key: keyGenerator(),
+    _type: 'block',
+    children: [
+      {
+        _key: keyGenerator(),
+        _type: 'span',
+        text: 'bar',
+      },
+    ],
+  }
+  const image = {
+    _key: keyGenerator(),
+    _type: 'image',
+  }
+  const imagePath = [{_key: image._key}]
+
+  function snapshot(selection: EditorSelection) {
+    return createTestSnapshot({
+      context: {
+        keyGenerator,
+        schema: compileSchemaDefinition(
+          defineSchema({blockObjects: [{name: 'image'}]}),
+        ),
+        selection,
+        value: [foo, bar, image],
+      },
+    })
+  }
+
+  test('dragging one text block', () => {
+    expect(
+      getDragSelection({
+        eventSelection: {
+          anchor: {
+            path: fooPath,
+            offset: 0,
+          },
+          focus: {
+            path: fooPath,
+            offset: 0,
+          },
+        },
+        snapshot: snapshot(null),
+      }),
+    ).toEqual({
+      anchor: {
+        path: fooPath,
+        offset: 0,
+      },
+      focus: {
+        path: fooPath,
+        offset: 3,
+      },
+    })
+  })
+
+  test('dragging one text block with an existing selection', () => {
+    expect(
+      getDragSelection({
+        eventSelection: {
+          anchor: {
+            path: fooPath,
+            offset: 0,
+          },
+          focus: {
+            path: fooPath,
+            offset: 0,
+          },
+        },
+        snapshot: snapshot({
+          anchor: {
+            path: fooPath,
+            offset: 0,
+          },
+          focus: {
+            path: fooPath,
+            offset: 3,
+          },
+        }),
+      }),
+    )
+  })
+
+  test('dragging one text block with a selection elsewhere', () => {
+    expect(
+      getDragSelection({
+        eventSelection: {
+          anchor: {
+            path: fooPath,
+            offset: 0,
+          },
+          focus: {
+            path: fooPath,
+            offset: 0,
+          },
+        },
+        snapshot: snapshot({
+          anchor: {
+            path: imagePath,
+            offset: 0,
+          },
+          focus: {
+            path: imagePath,
+            offset: 0,
+          },
+        }),
+      }),
+    ).toEqual({
+      anchor: {
+        path: fooPath,
+        offset: 0,
+      },
+      focus: {
+        path: fooPath,
+        offset: 3,
+      },
+    })
+  })
+
+  test('dragging one block object with a selection elsewhere', () => {
+    expect(
+      getDragSelection({
+        eventSelection: {
+          anchor: {
+            path: imagePath,
+            offset: 0,
+          },
+          focus: {
+            path: imagePath,
+            offset: 0,
+          },
+        },
+        snapshot: snapshot({
+          anchor: {
+            path: fooPath,
+            offset: 0,
+          },
+          focus: {
+            path: fooPath,
+            offset: 0,
+          },
+        }),
+      }),
+    ).toEqual({
+      anchor: {
+        path: imagePath,
+        offset: 0,
+      },
+      focus: {
+        path: imagePath,
+        offset: 0,
+      },
+    })
+  })
+
+  test('dragging a text block with an expanded selected', () => {
+    expect(
+      getDragSelection({
+        eventSelection: {
+          anchor: {
+            path: fooPath,
+            offset: 0,
+          },
+          focus: {
+            path: fooPath,
+            offset: 0,
+          },
+        },
+        snapshot: snapshot({
+          anchor: {
+            path: fooPath,
+            offset: 0,
+          },
+          focus: {
+            path: imagePath,
+            offset: 0,
+          },
+        }),
+      }),
+    ).toEqual({
+      anchor: {
+        path: fooPath,
+        offset: 0,
+      },
+      focus: {
+        path: imagePath,
+        offset: 0,
+      },
+    })
+  })
+
+  test('dragging a block object with an expanded selected', () => {
+    expect(
+      getDragSelection({
+        eventSelection: {
+          anchor: {
+            path: imagePath,
+            offset: 0,
+          },
+          focus: {
+            path: imagePath,
+            offset: 0,
+          },
+        },
+        snapshot: snapshot({
+          anchor: {
+            path: fooPath,
+            offset: 0,
+          },
+          focus: {
+            path: imagePath,
+            offset: 0,
+          },
+        }),
+      }),
+    ).toEqual({
+      anchor: {
+        path: fooPath,
+        offset: 0,
+      },
+      focus: {
+        path: imagePath,
+        offset: 0,
+      },
+    })
+  })
+})
---
+++ packages/editor/src/internal-utils/drag-selection.ts
@@ -0,0 +1,59 @@
+import type {EditorSnapshot} from '..'
+import * as selectors from '../selectors'
+import * as utils from '../utils'
+import type {EventPosition} from './event-position'
+
+export function getDragSelection({
+  eventSelection,
+  snapshot,
+}: {
+  eventSelection: EventPosition['selection']
+  snapshot: EditorSnapshot
+}) {
+  let dragSelection = eventSelection
+
+  const collapsedSelection = selectors.isSelectionCollapsed({
+    ...snapshot,
+    context: {
+      ...snapshot.context,
+      selection: eventSelection,
+    },
+  })
+  const focusTextBlock = selectors.getFocusTextBlock({
+    ...snapshot,
+    context: {
+      ...snapshot.context,
+      selection: eventSelection,
+    },
+  })
+  const focusSpan = selectors.getFocusSpan({
+    ...snapshot,
+    context: {
+      ...snapshot.context,
+      selection: eventSelection,
+    },
+  })
+
+  if (collapsedSelection && focusTextBlock && focusSpan) {
+    // Looks like we are dragging an empty span
+    // Let's drag the entire block instead
+    dragSelection = {
+      anchor: utils.getBlockStartPoint(focusTextBlock),
+      focus: utils.getBlockEndPoint(focusTextBlock),
+    }
+  }
+
+  if (snapshot.context.selection && selectors.isSelectionExpanded(snapshot)) {
+    const selectionStartBlock = selectors.getSelectionStartBlock(snapshot)
+    const selectionEndBlock = selectors.getSelectionEndBlock(snapshot)
+
+    if (selectionStartBlock && selectionEndBlock) {
+      dragSelection = {
+        anchor: utils.getBlockStartPoint(selectionStartBlock),
+        focus: utils.getBlockEndPoint(selectionEndBlock),
+      }
+    }
+  }
+
+  return dragSelection
+}
