8e728bae62dc8094b4c75168729a9a8944ce153e
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-03-20 10:56:27 +0100

    chore: add `getNodeBlock` Slate util

--- packages/editor/src/internal-utils/slate-utils.ts
+++ packages/editor/src/internal-utils/slate-utils.ts
@@ -1,5 +1,7 @@
-import {Editor, Node, type Path} from 'slate'
+import {Editor, Element, Node, type Path} from 'slate'
+import type {EditorSchema} from '../editor/define-schema'
 import type {PortableTextSlateEditor} from '../types/editor'
+import {fromSlateValue} from './values'
 
 export function getFocusBlock({
   editor,
@@ -53,4 +55,61 @@ export function getLastBlock({
   ).at(0)
 
   return lastBlock ?? [undefined, undefined]
+}
+
+export function getNodeBlock({
+  editor,
+  schema,
+  node,
+}: {
+  editor: PortableTextSlateEditor
+  schema: EditorSchema
+  node: Node
+}) {
+  if (Editor.isEditor(node)) {
+    return undefined
+  }
+
+  if (isBlockElement(schema, node)) {
+    return elementToBlock({schema, element: node})
+  }
+
+  const parent = Array.from(
+    Editor.nodes(editor, {
+      mode: 'highest',
+      at: [],
+      match: (n) =>
+        isBlockElement(schema, n) &&
+        n.children.some((child) => child._key === node._key),
+    }),
+  )
+    .at(0)
+    ?.at(0)
+
+  return Element.isElement(parent)
+    ? elementToBlock({
+        schema,
+        element: parent,
+      })
+    : undefined
+}
+
+function elementToBlock({
+  schema,
+  element,
+}: {
+  schema: EditorSchema
+  element: Element
+}) {
+  return fromSlateValue([element], schema.block.name)?.at(0)
+}
+
+function isBlockElement(schema: EditorSchema, node: Node): node is Element {
+  return (
+    Element.isElement(node) &&
+    (schema.block.name === node._type ||
+      schema.blockObjects.some(
+        (blockObject) => blockObject.name === node._type,
+      ))
+  )
 }
