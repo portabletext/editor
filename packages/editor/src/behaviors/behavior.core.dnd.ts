8e390bd08c506ee7c931d919cfdd4d801421de5f
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-03-22 16:45:06 +0100

    fix(dnd): prevent `drag.dragover` default behavior using Behavior

---
+++ packages/editor/src/behaviors/behavior.core.dnd.ts
@@ -0,0 +1,27 @@
+import * as selectors from '../selectors'
+import {defineBehavior} from './behavior.types'
+
+export const coreDndBehaviors = [
+  /**
+   * When dragging over the drag origin, we don't want to show the caret in the
+   * text.
+   */
+  defineBehavior({
+    on: 'drag.dragover',
+    guard: ({snapshot, event}) => {
+      const dragOrigin = snapshot.beta.internalDrag?.origin
+      const draggingOverDragOrigin = dragOrigin
+        ? selectors.isOverlappingSelection(event.position.selection)({
+            ...snapshot,
+            context: {
+              ...snapshot.context,
+              selection: dragOrigin.selection,
+            },
+          })
+        : false
+
+      return draggingOverDragOrigin
+    },
+    actions: [() => [{type: 'noop'}]],
+  }),
+]
