1b98f185581f997b66bfa30946266df087757fdc
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-04-22 09:50:01 +0200

    refactor: rename `block-objects.insert-block.feature`->`insert.block.feature`

--- packages/editor/gherkin-tests-v2/insert.block.test.ts
+++ packages/editor/gherkin-tests-v2/insert.block.test.ts
@@ -1,10 +1,10 @@
 import {Feature} from 'racejar/vitest'
-import blockObjectsInsertBlockFeature from '../gherkin-spec/block-objects.insert-block.feature?raw'
+import insertBlockFeature from '../gherkin-spec/insert.block.feature?raw'
 import {parameterTypes} from './gherkin-parameter-types'
 import {stepDefinitions} from './step-definitions'
 
 Feature({
-  featureText: blockObjectsInsertBlockFeature,
+  featureText: insertBlockFeature,
   stepDefinitions,
   parameterTypes,
 })
