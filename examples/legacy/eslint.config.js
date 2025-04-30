99e2a7686e949de96b43cf7807b6824fcaec33cd
Cody Olsen <stipsan@gmail.com>
2025-04-30 15:09:20 +0200

    fix(deps): upgrade to react compiler RC

--- examples/legacy/eslint.config.js
+++ examples/legacy/eslint.config.js
@@ -1,5 +1,4 @@
 import js from '@eslint/js'
-import reactCompiler from 'eslint-plugin-react-compiler'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
@@ -15,17 +14,17 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
-      'react-compiler': reactCompiler,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'react-hooks/exhaustive-deps': 'error',
       'react-refresh/only-export-components': [
         'warn',
         {allowConstantExport: true},
       ],
-      'react-compiler/react-compiler': 'error',
+      'react-hooks/react-compiler': 'error',
     },
   },
 )
