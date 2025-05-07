37113bfd07cba8463e721ea0dbfdc1575a0bb41a
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-05-07 13:34:08 +0200

    fix: don't validate fields when dragging internally

--- packages/editor/src/converters/converter.portable-text.ts
+++ packages/editor/src/converters/converter.portable-text.ts
@@ -62,7 +62,7 @@ export const converterPortableText = defineConverter({
            * dropped portable text as is.
            */
           refreshKeys: !snapshot.beta.hasTag?.('dragging internally'),
-          validateFields: true,
+          validateFields: false,
         },
       })
       return parsedBlock ? [parsedBlock] : []
