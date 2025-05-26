e705780913de1dbc8b322e109f911c19a6051cec
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-05-26 09:11:43 +0200

    fix(behaviors): turn `delete.block` into an Abstract Event

---
+++ packages/editor/tests/event.delete.block.test.tsx
@@ -0,0 +1,92 @@
+import {describe, expect, test, vi} from 'vitest'
+import {defineSchema} from '../src/editor/editor-schema'
+import {createTestEditor} from '../src/internal-utils/test-editor'
+import {createTestKeyGenerator} from '../src/internal-utils/test-key-generator'
+
+describe('event.delete.block', () => {
+  test('Scenario: Deleting lonely block object', async () => {
+    const keyGenerator = createTestKeyGenerator()
+    const imageKey = keyGenerator()
+    const {editorRef} = await createTestEditor({
+      keyGenerator,
+      initialValue: [
+        {
+          _type: 'image',
+          _key: imageKey,
+        },
+      ],
+      schemaDefinition: defineSchema({
+        blockObjects: [{name: 'image'}],
+      }),
+    })
+
+    await vi.waitFor(() => {
+      expect(editorRef.current?.getSnapshot().context.value).toEqual([
+        {_type: 'image', _key: imageKey},
+      ])
+    })
+
+    editorRef.current?.send({
+      type: 'delete.block',
+      at: [{_key: imageKey}],
+    })
+
+    await vi.waitFor(() => {
+      expect(editorRef.current?.getSnapshot().context.value).toEqual([
+        {
+          _type: 'block',
+          _key: 'k3',
+          children: [{_type: 'span', _key: 'k4', text: '', marks: []}],
+          markDefs: [],
+          style: 'normal',
+        },
+      ])
+    })
+  })
+
+  test('Scenario: Deleting lonely text block', async () => {
+    const keyGenerator = createTestKeyGenerator()
+    const blockKey = keyGenerator()
+    const spanKey = keyGenerator()
+    const {editorRef} = await createTestEditor({
+      keyGenerator,
+      initialValue: [
+        {
+          _type: 'block',
+          _key: blockKey,
+          children: [
+            {
+              _type: 'span',
+              _key: spanKey,
+              text: 'foo',
+              marks: [],
+            },
+          ],
+          markDefs: [],
+          style: 'normal',
+        },
+      ],
+    })
+
+    await vi.waitFor(() => {
+      expect(editorRef.current?.getSnapshot().context.value).toEqual([
+        {
+          _type: 'block',
+          _key: blockKey,
+          children: [{_type: 'span', _key: spanKey, text: 'foo', marks: []}],
+          markDefs: [],
+          style: 'normal',
+        },
+      ])
+    })
+
+    editorRef.current?.send({
+      type: 'delete.block',
+      at: [{_key: blockKey}],
+    })
+
+    await vi.waitFor(() => {
+      expect(editorRef.current?.getSnapshot().context.value).toEqual([])
+    })
+  })
+})
