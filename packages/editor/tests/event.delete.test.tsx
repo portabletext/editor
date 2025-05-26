4a1580203115ed08ddafdc4ec179bed735e412cc
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-05-26 13:14:08 +0200

    test(`delete`): deleting entire editor

--- packages/editor/tests/event.delete.test.tsx
+++ packages/editor/tests/event.delete.test.tsx
@@ -7,6 +7,7 @@ import {PortableTextEditable} from '../src/editor/Editable'
 import {EditorProvider} from '../src/editor/editor-provider'
 import {defineSchema} from '../src/editor/editor-schema'
 import {getTersePt} from '../src/internal-utils/terse-pt'
+import {createTestEditor} from '../src/internal-utils/test-editor'
 import {createTestKeyGenerator} from '../src/internal-utils/test-key-generator'
 import {EditorRefPlugin} from '../src/plugins/plugin.editor-ref'
 
@@ -104,6 +105,67 @@ describe('event.delete', () => {
       expect(
         getTersePt(editorRef.current?.getSnapshot().context.value),
       ).toEqual(['foobar'])
+    })
+  })
+
+  test('Scenario: Deleting entire editor', async () => {
+    const keyGenerator = createTestKeyGenerator()
+    const fooBlockKey = keyGenerator()
+    const fooSpanKey = keyGenerator()
+    const barBlockKey = keyGenerator()
+    const barSpanKey = keyGenerator()
+    const imageKey = keyGenerator()
+    const {editorRef} = await createTestEditor({
+      initialValue: [
+        {
+          _type: 'block',
+          _key: fooBlockKey,
+          children: [{_type: 'span', _key: fooSpanKey, text: 'foo', marks: []}],
+          markDefs: [],
+          style: 'normal',
+        },
+        {
+          _type: 'image',
+          _key: imageKey,
+        },
+        {
+          _type: 'block',
+          _key: barBlockKey,
+          children: [{_type: 'span', _key: barSpanKey, text: 'bar', marks: []}],
+          markDefs: [],
+          style: 'normal',
+        },
+      ],
+      keyGenerator,
+      schemaDefinition: defineSchema({
+        blockObjects: [{name: 'image'}],
+      }),
+    })
+
+    await vi.waitFor(() => {
+      expect(
+        getTersePt(editorRef.current?.getSnapshot().context.value),
+      ).toEqual(['foo', '|', '[image]', '|', 'bar'])
+    })
+
+    editorRef.current?.send({
+      type: 'delete',
+      at: {
+        anchor: {
+          path: [{_key: fooBlockKey}, 'children', {_key: fooSpanKey}],
+          offset: 0,
+        },
+        focus: {
+          path: [{_key: barBlockKey}, 'children', {_key: barSpanKey}],
+          offset: 3,
+        },
+      },
+    })
+
+    await vi.waitFor(() => {
+      expect(
+        getTersePt(editorRef.current?.getSnapshot().context.value),
+      ).toEqual([''])
     })
   })
 })
