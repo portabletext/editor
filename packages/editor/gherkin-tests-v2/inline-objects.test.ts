cc188ace61b8d8d7b80be70efec2a5dfdf321182
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-06-02 09:49:42 +0200

    test: port `inline-objects.feature` to new test suite

--- packages/editor/gherkin-tests-v2/inline-objects.test.ts
+++ packages/editor/gherkin-tests-v2/inline-objects.test.ts
@@ -1,7 +1,7 @@
 import {Feature} from 'racejar/vitest'
 import inlineObjectsFeature from '../gherkin-spec/inline-objects.feature?raw'
 import {parameterTypes} from './gherkin-parameter-types'
-import {stepDefinitions} from './gherkin-step-definitions'
+import {stepDefinitions} from './step-definitions'
 
 Feature({
   featureText: inlineObjectsFeature,
