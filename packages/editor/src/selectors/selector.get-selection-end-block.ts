6abb125dd3df3a01701d8e3c4fdb372a8fd15f01
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-06-04 23:16:27 +0200

    chore: add `getSelection(Start|End)Block selectors

---
+++ packages/editor/src/selectors/selector.get-selection-end-block.ts
@@ -0,0 +1,30 @@
+import type {PortableTextBlock} from '@sanity/types'
+import type {EditorSelector} from '../editor/editor-selector'
+import type {BlockPath} from '../types/paths'
+import {getSelectionEndPoint} from '../utils/util.get-selection-end-point'
+import {getFocusBlock} from './selectors'
+
+export const getSelectionEndBlock: EditorSelector<
+  | {
+      node: PortableTextBlock
+      path: BlockPath
+    }
+  | undefined
+> = (snapshot) => {
+  const endPoint = getSelectionEndPoint(snapshot.context.selection)
+
+  if (!endPoint) {
+    return undefined
+  }
+
+  return getFocusBlock({
+    ...snapshot,
+    context: {
+      ...snapshot.context,
+      selection: {
+        anchor: endPoint,
+        focus: endPoint,
+      },
+    },
+  })
+}
---
+++ packages/editor/src/selectors/selector.get-selection-start-block.ts
@@ -0,0 +1,30 @@
+import type {PortableTextBlock} from '@sanity/types'
+import type {EditorSelector} from '../editor/editor-selector'
+import type {BlockPath} from '../types/paths'
+import {getSelectionStartPoint} from '../utils/util.get-selection-start-point'
+import {getFocusBlock} from './selectors'
+
+export const getSelectionStartBlock: EditorSelector<
+  | {
+      node: PortableTextBlock
+      path: BlockPath
+    }
+  | undefined
+> = (snapshot) => {
+  const startPoint = getSelectionStartPoint(snapshot.context.selection)
+
+  if (!startPoint) {
+    return undefined
+  }
+
+  return getFocusBlock({
+    ...snapshot,
+    context: {
+      ...snapshot.context,
+      selection: {
+        anchor: startPoint,
+        focus: startPoint,
+      },
+    },
+  })
+}
