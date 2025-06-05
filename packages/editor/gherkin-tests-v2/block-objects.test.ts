03b1c46b6304722f990e1b5cd61514eceb1da87d
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-06-05 18:01:33 +0200

    test: port `block-objects.feature` to new test suite

--- packages/editor/gherkin-tests-v2/block-objects.test.ts
+++ packages/editor/gherkin-tests-v2/block-objects.test.ts
@@ -1,7 +1,7 @@
 import {Feature} from 'racejar/vitest'
 import blockObjectsFeature from '../gherkin-spec/block-objects.feature?raw'
 import {parameterTypes} from './gherkin-parameter-types'
-import {stepDefinitions} from './gherkin-step-definitions'
+import {stepDefinitions} from './step-definitions'
 
 Feature({
   featureText: blockObjectsFeature,
