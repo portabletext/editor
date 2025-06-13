bd0776c50b0931fc48287b149790cbc20a92c6a5
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-06-13 09:35:06 +0200

    fix: raise `insert.break` when hitting Enter on inline object

--- packages/editor/src/behaviors/behavior.abstract.keyboard.ts
+++ packages/editor/src/behaviors/behavior.abstract.keyboard.ts
@@ -1,8 +1,18 @@
 import {keyIs} from '../internal-utils/key-is'
+import * as selectors from '../selectors'
 import {raise} from './behavior.types.action'
 import {defineBehavior} from './behavior.types.behavior'
 
 export const abstractKeyboardBehaviors = [
+  defineBehavior({
+    on: 'keyboard.keydown',
+    guard: ({snapshot, event}) =>
+      keyIs.break(event.originEvent) &&
+      selectors.isSelectionCollapsed(snapshot) &&
+      selectors.getFocusInlineObject(snapshot),
+    actions: [() => [raise({type: 'insert.break'})]],
+  }),
+
   /**
    * On WebKit, Shift+Enter results in an `insertParagraph` input event rather
    * than an `insertLineBreak` input event. This Behavior makes sure we catch
