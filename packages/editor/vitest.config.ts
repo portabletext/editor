81cd17bbd293588ae2c18ede0b25a8cdab4d6013
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-06-13 13:00:13 +0200

    test: remove usage of deprecated Vitest workspaces

--- packages/editor/vitest.config.ts
+++ packages/editor/vitest.config.ts
@@ -1,3 +1,4 @@
+import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vitest/config'
 
 export default defineConfig({
@@ -9,5 +10,93 @@ export default defineConfig({
       reportOnFailure: true,
       clean: true,
     },
+    projects: [
+      {
+        plugins: [
+          react({
+            babel: {plugins: [['babel-plugin-react-compiler', {target: '18'}]]},
+          }),
+        ],
+        test: {
+          name: 'browser.bak',
+          include: [
+            'gherkin-tests/**/*.test.ts',
+            'gherkin-tests/**/*.test.tsx',
+          ],
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: 'playwright',
+            instances: [
+              {
+                browser: 'chromium',
+              },
+              {
+                browser: 'firefox',
+              },
+              {
+                browser: 'webkit',
+              },
+            ],
+            screenshotFailures: false,
+          },
+        },
+      },
+      {
+        plugins: [
+          react({
+            babel: {plugins: [['babel-plugin-react-compiler', {target: '18'}]]},
+          }),
+        ],
+        test: {
+          name: 'browser',
+          include: [
+            'gherkin-tests-v2/**/*.test.ts',
+            'gherkin-tests-v2/**/*.test.tsx',
+            'tests/**/*.test.ts',
+            'tests/**/*.test.tsx',
+            'src/plugins/*.test.tsx',
+          ],
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: 'playwright',
+            instances: [
+              {
+                browser: 'chromium',
+              },
+              {
+                browser: 'firefox',
+                retry: 3,
+              },
+              {
+                browser: 'webkit',
+              },
+            ],
+            screenshotFailures: false,
+          },
+        },
+      },
+      {
+        plugins: [
+          react({
+            babel: {plugins: [['babel-plugin-react-compiler', {target: '18'}]]},
+          }),
+        ],
+        test: {
+          name: 'unit',
+          exclude: [
+            'node_modules',
+            'e2e-tests',
+            'gherkin-tests',
+            'gherkin-tests-v2',
+            'tests',
+            'src/plugins/*.test.tsx',
+          ],
+          environment: 'jsdom',
+          setupFiles: ['./vitest.setup.ts'],
+        },
+      },
+    ],
   },
 })
