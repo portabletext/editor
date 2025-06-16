d1f3c77750b6c156dc9d4d77f097e67ad9f30741
raptoria <tina.lacey@gmail.com>
2025-06-16 10:15:08 -0400

    resolve pnpm conflict

--- examples/advanced/src/components/theme-dropdown.tsx
+++ examples/advanced/src/components/theme-dropdown.tsx
@@ -14,7 +14,7 @@ export function ThemeDropdown() {
   return (
     <StyledDropdownMenu>
       <StyledDropdownMenuTrigger asChild>
-        <StyledButton variant="ghost" className="h-8 w-8 px-0">
+        <StyledButton variant="ghost" className="h-8 w-8 px-0 ml-2">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
