d8bb8038ab339899790d63a33cee5a89999cefd5
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-06-18 16:14:24 +0200

    feat: add `annotation.set` event

--- packages/editor/src/behaviors/behavior.abstract.annotation.ts
+++ packages/editor/src/behaviors/behavior.abstract.annotation.ts
@@ -1,8 +1,59 @@
 import {isActiveAnnotation} from '../selectors'
+import * as selectors from '../selectors'
 import {raise} from './behavior.types.action'
 import {defineBehavior} from './behavior.types.behavior'
 
 export const abstractAnnotationBehaviors = [
+  defineBehavior({
+    on: 'annotation.set',
+    guard: ({snapshot, event}) => {
+      const blockKey = event.at[0]._key
+      const markDefKey = event.at[2]._key
+
+      const block = selectors.getFocusTextBlock({
+        ...snapshot,
+        context: {
+          ...snapshot.context,
+          selection: {
+            anchor: {
+              path: [{_key: blockKey}],
+              offset: 0,
+            },
+            focus: {
+              path: [{_key: blockKey}],
+              offset: 0,
+            },
+          },
+        },
+      })
+
+      if (!block) {
+        return false
+      }
+
+      const updatedMarkDefs = block.node.markDefs?.map((markDef) => {
+        if (markDef._key === markDefKey) {
+          return {
+            ...markDef,
+            ...event.props,
+          }
+        }
+
+        return markDef
+      })
+
+      return {blockKey, updatedMarkDefs}
+    },
+    actions: [
+      (_, {blockKey, updatedMarkDefs}) => [
+        raise({
+          type: 'block.set',
+          at: [{_key: blockKey}],
+          props: {markDefs: updatedMarkDefs},
+        }),
+      ],
+    ],
+  }),
   defineBehavior({
     on: 'annotation.toggle',
     guard: ({snapshot, event}) =>
--- packages/editor/src/types/paths.ts
+++ packages/editor/src/types/paths.ts
@@ -1,1 +1,6 @@
 export type BlockPath = [{_key: string}]
+
+/**
+ * @public
+ */
+export type AnnotationPath = [{_key: string}, 'markDefs', {_key: string}]
