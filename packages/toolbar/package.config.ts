2274d1626c871c8cfc383a789ba8921d8fbb16c0
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-06-30 12:45:06 +0200

    chore: set up React Compiler

--- packages/toolbar/package.config.ts
+++ packages/toolbar/package.config.ts
@@ -2,4 +2,6 @@ import {defineConfig} from '@sanity/pkg-utils'
 
 export default defineConfig({
   tsconfig: 'tsconfig.dist.json',
+  babel: {reactCompiler: true},
+  reactCompilerOptions: {target: '19'},
 })
