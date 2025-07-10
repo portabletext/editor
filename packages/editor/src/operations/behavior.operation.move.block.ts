1a7e20d05730c7b97abab54087edde0b471668af
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-07-10 08:39:53 +0200

    refactor: simplify `move.block` operation

--- packages/editor/src/operations/behavior.operation.move.block.ts
+++ packages/editor/src/operations/behavior.operation.move.block.ts
@@ -1,38 +1,44 @@
 import {Transforms} from 'slate'
-import {toSlatePath} from '../internal-utils/paths'
+import {getBlockKeyFromSelectionPoint} from '../selection/selection-point'
 import type {BehaviorOperationImplementation} from './behavior.operations'
 
 export const moveBlockOperationImplementation: BehaviorOperationImplementation<
   'move.block'
-> = ({context, operation}) => {
-  const at = [
-    toSlatePath(
-      {
-        context: {
-          schema: context.schema,
-          value: operation.editor.value,
-        },
-        blockIndexMap: operation.editor.blockIndexMap,
-      },
-      operation.at,
-    ).path[0],
-  ]
-  const to = [
-    toSlatePath(
-      {
-        context: {
-          schema: context.schema,
-          value: operation.editor.value,
-        },
-        blockIndexMap: operation.editor.blockIndexMap,
-      },
-      operation.to,
-    ).path[0],
-  ]
+> = ({operation}) => {
+  const originKey = getBlockKeyFromSelectionPoint({
+    path: operation.at,
+    offset: 0,
+  })
+
+  if (!originKey) {
+    throw new Error('Failed to get block key from selection point')
+  }
+
+  const originBlockIndex = operation.editor.blockIndexMap.get(originKey)
+
+  if (originBlockIndex === undefined) {
+    throw new Error('Failed to get block index from block key')
+  }
+
+  const destinationKey = getBlockKeyFromSelectionPoint({
+    path: operation.to,
+    offset: 0,
+  })
+
+  if (!destinationKey) {
+    throw new Error('Failed to get block key from selection point')
+  }
+
+  const destinationBlockIndex =
+    operation.editor.blockIndexMap.get(destinationKey)
+
+  if (destinationBlockIndex === undefined) {
+    throw new Error('Failed to get block index from block key')
+  }
 
   Transforms.moveNodes(operation.editor, {
-    at,
-    to,
+    at: [originBlockIndex],
+    to: [destinationBlockIndex],
     mode: 'highest',
   })
 }
