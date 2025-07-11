99d2728f4b3f49804503a2b7436dd8bb6df87fcf
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-07-11 08:45:27 +0200

    chore: remove unused function

--- packages/editor/src/editor/editor-dom.ts
+++ packages/editor/src/editor/editor-dom.ts
@@ -99,62 +99,6 @@ function getChildNodes(
   }
 }
 
-export type SelectionDomNodes = {
-  blockNodes: Array<Node>
-  childNodes: Array<Node>
-}
-
-export function getSelectionDomNodes({
-  slateEditor,
-  snapshot,
-}: {
-  slateEditor: PortableTextSlateEditor
-  snapshot: EditorSnapshot
-}): SelectionDomNodes {
-  if (!snapshot.context.selection) {
-    return {
-      blockNodes: [],
-      childNodes: [],
-    }
-  }
-
-  const range = toSlateRange(snapshot)
-
-  if (!range) {
-    return {
-      blockNodes: [],
-      childNodes: [],
-    }
-  }
-
-  const blockEntries = Array.from(
-    Editor.nodes(slateEditor, {
-      at: range,
-      mode: 'highest',
-      match: (n) => !Editor.isEditor(n),
-    }),
-  )
-
-  const childEntries = Array.from(
-    Editor.nodes(slateEditor, {
-      at: range,
-      mode: 'lowest',
-      match: (n) =>
-        (!Editor.isEditor(n) && slateEditor.isTextSpan(n)) ||
-        !slateEditor.isBlock(n),
-    }),
-  )
-
-  return {
-    blockNodes: blockEntries.map(([blockNode]) =>
-      DOMEditor.toDOMNode(slateEditor, blockNode),
-    ),
-    childNodes: childEntries.map(([childNode]) =>
-      DOMEditor.toDOMNode(slateEditor, childNode),
-    ),
-  }
-}
-
 function setDragGhost({
   sendBack,
   event,
