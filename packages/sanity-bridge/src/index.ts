264ee67cdbe0ee42e36d1bb575a1b434cd38615e
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-08-13 10:26:49 +0200

    feat: add `sanitySchemaToPortableTextSchema`

--- packages/sanity-bridge/src/index.ts
+++ packages/sanity-bridge/src/index.ts
@@ -3,4 +3,5 @@ export {
   type PortableTextMemberSchemaTypes,
 } from './portable-text-member-schema-types'
 export {portableTextMemberSchemaTypesToSchema} from './portable-text-member-schema-types-to-schema'
+export {sanitySchemaToPortableTextSchema} from './sanity-schema-to-portable-text-schema'
 export {compileSchemaDefinitionToPortableTextMemberSchemaTypes} from './schema-definition-to-portable-text-member-schema-types'
---
+++ packages/sanity-bridge/src/sanity-schema-to-portable-text-schema.ts
@@ -0,0 +1,51 @@
+import type {Schema} from '@portabletext/schema'
+import {Schema as SanitySchema} from '@sanity/schema'
+import type {
+  ArrayDefinition,
+  ArraySchemaType,
+  PortableTextBlock,
+} from '@sanity/types'
+import {createPortableTextMemberSchemaTypes} from './portable-text-member-schema-types'
+import {portableTextMemberSchemaTypesToSchema} from './portable-text-member-schema-types-to-schema'
+
+/**
+ * @public
+ * Compile a Sanity schema to a Portable Text `Schema`.
+ *
+ * A Portable Text `Schema` is compatible with a Portable Text
+ * `SchemaDefinition` and can be used as configuration for the Portable Text
+ * Editor.
+ *
+ * @example
+ * ```tsx
+ * const schema = sanitySchemaToPortableTextSchema(sanitySchema)
+ *
+ * return (
+ *   <EditorProvider
+ *     initialConfig={{
+ *       // ...
+ *       schemaDefinition: schema,
+ *     }}
+ *   >
+ *     // ...
+ *   </EditorProvider>
+ * ```
+ */
+export function sanitySchemaToPortableTextSchema(
+  sanitySchema: ArraySchemaType<PortableTextBlock> | ArrayDefinition,
+): Schema {
+  const portableTextMemberSchemaTypes = createPortableTextMemberSchemaTypes(
+    sanitySchema.hasOwnProperty('jsonType')
+      ? sanitySchema
+      : compileType(sanitySchema),
+  )
+
+  return portableTextMemberSchemaTypesToSchema(portableTextMemberSchemaTypes)
+}
+
+function compileType(rawType: any) {
+  return SanitySchema.compile({
+    name: 'blockTypeSchema',
+    types: [rawType],
+  }).get(rawType.name)
+}
