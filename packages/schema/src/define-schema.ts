54a6b47f5e1757cfb43fc04969ae4885b8146a4c
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-08-13 09:05:44 +0200

    feat: introduce `@portabletext/schema` package

---
+++ packages/schema/src/define-schema.ts
@@ -0,0 +1,93 @@
+import type {BaseDefinition, FieldDefinition} from './schema'
+
+/**
+ * @public
+ */
+export type SchemaDefinition = {
+  block?: {
+    name: string
+  }
+  styles?: ReadonlyArray<StyleDefinition>
+  lists?: ReadonlyArray<ListDefinition>
+  decorators?: ReadonlyArray<DecoratorDefinition>
+  annotations?: ReadonlyArray<AnnotationDefinition>
+  blockObjects?: ReadonlyArray<BlockObjectDefinition>
+  inlineObjects?: ReadonlyArray<InlineObjectDefinition>
+}
+
+/**
+ * @public
+ * A helper wrapper that adds editor support, such as autocomplete and type checking, for a schema definition.
+ * @example
+ * ```ts
+ * import { defineSchema } from '@portabletext/editor'
+ *
+ * const schemaDefinition = defineSchema({
+ *  decorators: [{name: 'strong'}, {name: 'em'}, {name: 'underline'}],
+ *  annotations: [{name: 'link'}],
+ *  styles: [
+ *    {name: 'normal'},
+ *    {name: 'h1'},
+ *    {name: 'h2'},
+ *    {name: 'h3'},
+ *    {name: 'blockquote'},
+ *  ],
+ *  lists: [],
+ *  inlineObjects: [],
+ *  blockObjects: [],
+ * }
+ * ```
+ */
+export function defineSchema<const TSchemaDefinition extends SchemaDefinition>(
+  definition: TSchemaDefinition,
+): TSchemaDefinition {
+  return definition
+}
+
+/**
+ * @public
+ */
+export type StyleDefinition<
+  TBaseDefinition extends BaseDefinition = BaseDefinition,
+> = TBaseDefinition
+
+/**
+ * @public
+ */
+export type ListDefinition<
+  TBaseDefinition extends BaseDefinition = BaseDefinition,
+> = TBaseDefinition
+
+/**
+ * @public
+ */
+export type DecoratorDefinition<
+  TBaseDefinition extends BaseDefinition = BaseDefinition,
+> = TBaseDefinition
+
+/**
+ * @public
+ */
+export type AnnotationDefinition<
+  TBaseDefinition extends BaseDefinition = BaseDefinition,
+> = TBaseDefinition & {
+  fields?: ReadonlyArray<FieldDefinition>
+}
+
+/**
+ * @public
+ */
+export type BlockObjectDefinition<
+  TBaseDefinition extends BaseDefinition = BaseDefinition,
+> = TBaseDefinition & {
+  fields?: ReadonlyArray<FieldDefinition>
+}
+
+/**
+ * @public
+ */
+export type InlineObjectDefinition<
+  TBaseDefinition extends BaseDefinition = BaseDefinition,
+> = TBaseDefinition & {
+  fields?: ReadonlyArray<FieldDefinition>
+}
---
+++ packages/schema/src/schema.ts
@@ -0,0 +1,86 @@
+/**
+ * @public
+ */
+export type Schema = {
+  block: {
+    name: string
+  }
+  span: {
+    name: string
+  }
+  styles: ReadonlyArray<StyleSchemaType>
+  lists: ReadonlyArray<ListSchemaType>
+  decorators: ReadonlyArray<DecoratorSchemaType>
+  annotations: ReadonlyArray<AnnotationSchemaType>
+  blockObjects: ReadonlyArray<BlockObjectSchemaType>
+  inlineObjects: ReadonlyArray<InlineObjectSchemaType>
+}
+
+/**
+ * @public
+ */
+export type StyleSchemaType = BaseDefinition & {
+  /**
+   * @deprecated
+   * Use `name` instead
+   */
+  value: string
+}
+
+/**
+ * @public
+ */
+export type ListSchemaType = BaseDefinition & {
+  /**
+   * @deprecated
+   * Use `name` instead
+   */
+  value: string
+}
+
+/**
+ * @public
+ */
+export type DecoratorSchemaType = BaseDefinition & {
+  /**
+   * @deprecated
+   * Use `name` instead
+   */
+  value: string
+}
+
+/**
+ * @public
+ */
+export type AnnotationSchemaType = BaseDefinition & {
+  fields: ReadonlyArray<FieldDefinition>
+}
+
+/**
+ * @public
+ */
+export type BlockObjectSchemaType = BaseDefinition & {
+  fields: ReadonlyArray<FieldDefinition>
+}
+
+/**
+ * @public
+ */
+export type InlineObjectSchemaType = BaseDefinition & {
+  fields: ReadonlyArray<FieldDefinition>
+}
+
+/**
+ * @public
+ */
+export type FieldDefinition = BaseDefinition & {
+  type: 'string' | 'number' | 'boolean' | 'array' | 'object'
+}
+
+/**
+ * @public
+ */
+export type BaseDefinition = {
+  name: string
+  title?: string
+}
