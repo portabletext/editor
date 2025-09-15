681c24355aa52acadbc986e9466c978c4857ad72
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-09-15 10:38:14 +0200

    refactor: simplify `blockSetOperationImplementation`

--- packages/editor/src/operations/behavior.operation.block.set.ts
+++ packages/editor/src/operations/behavior.operation.block.set.ts
@@ -1,64 +1,45 @@
-import {Editor, Transforms, type Element as SlateElement} from 'slate'
+import {Transforms, type Element as SlateElement} from 'slate'
 import {parseBlock} from '../internal-utils/parse-blocks'
-import {toSlateRange} from '../internal-utils/to-slate-range'
-import {fromSlateValue, toSlateValue} from '../internal-utils/values'
-import {KEY_TO_VALUE_ELEMENT} from '../internal-utils/weakMaps'
+import {toSlateValue} from '../internal-utils/values'
 import type {BehaviorOperationImplementation} from './behavior.operations'
 
 export const blockSetOperationImplementation: BehaviorOperationImplementation<
   'block.set'
 > = ({context, operation}) => {
-  const location = toSlateRange({
-    context: {
-      schema: context.schema,
-      value: operation.editor.value,
-      selection: {
-        anchor: {path: operation.at, offset: 0},
-        focus: {path: operation.at, offset: 0},
-      },
-    },
-    blockIndexMap: operation.editor.blockIndexMap,
-  })
+  const blockIndex = operation.editor.blockIndexMap.get(operation.at[0]._key)
 
-  if (!location) {
+  if (blockIndex === undefined) {
     throw new Error(
-      `Unable to convert ${JSON.stringify(operation.at)} into a Slate Range`,
+      `Unable to find block index for block at ${JSON.stringify(operation.at)}`,
     )
   }
 
-  const blockEntry = Editor.node(operation.editor, location, {depth: 1})
-  const block = blockEntry?.[0]
+  const block = operation.editor.value.at(blockIndex)
 
   if (!block) {
     throw new Error(`Unable to find block at ${JSON.stringify(operation.at)}`)
   }
 
-  const parsedBlock = fromSlateValue(
-    [block],
-    context.schema.block.name,
-    KEY_TO_VALUE_ELEMENT.get(operation.editor),
-  ).at(0)
-
-  if (!parsedBlock) {
-    throw new Error(`Unable to parse block at ${JSON.stringify(operation.at)}`)
-  }
-
   const {_type, ...filteredProps} = operation.props
 
-  const updatedBlock = parseBlock({
+  const updatedBlock = {
+    ...block,
+    ...filteredProps,
+  }
+
+  const parsedBlock = parseBlock({
     context,
-    block: {
-      ...parsedBlock,
-      ...filteredProps,
-    },
+    block: updatedBlock,
     options: {validateFields: true},
   })
 
-  if (!updatedBlock) {
+  if (!parsedBlock) {
     throw new Error(`Unable to update block at ${JSON.stringify(operation.at)}`)
   }
 
-  const slateBlock = toSlateValue([updatedBlock], {
+  parsedBlock.markDefs = updatedBlock.markDefs
+
+  const slateBlock = toSlateValue([parsedBlock], {
     schemaTypes: context.schema,
   })?.at(0) as SlateElement | undefined
 
@@ -66,5 +47,5 @@ export const blockSetOperationImplementation: BehaviorOperationImplementation<
     throw new Error(`Unable to convert block to Slate value`)
   }
 
-  Transforms.setNodes(operation.editor, slateBlock, {at: location})
+  Transforms.setNodes(operation.editor, slateBlock, {at: [blockIndex]})
 }
