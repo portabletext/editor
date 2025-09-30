c2cff8d4b254540aea5870b12f6c686ed73bff8e
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-09-30 11:47:39 +0200

    chore: run `check:react-compiler` on all packages that have it defined

--- packages/plugin-input-rule/eslint.config.js
+++ packages/plugin-input-rule/eslint.config.js
@@ -3,7 +3,7 @@ import {globalIgnores} from 'eslint/config'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config([
-  globalIgnores(['dist', 'lib', '**/__tests__/**']),
+  globalIgnores(['dist', 'lib', '**/*.test.*']),
   ...reactHooks.configs['flat/recommended'],
   {
     files: ['src/**/*.{cjs,mjs,js,jsx,ts,tsx}'],
