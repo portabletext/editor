49350ddf4691690c6a42f93f70f73cd56752849c
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-10-03 09:13:28 +0200

    refactor: move `defineTextTransformRule` to its own file

---
+++ packages/plugin-input-rule/src/text-transform-rule.ts
@@ -0,0 +1,78 @@
+import {raise} from '@portabletext/editor/behaviors'
+import {getMarkState} from '@portabletext/editor/selectors'
+import type {InputRule} from './input-rule'
+
+type TextTransformRule = {
+  matcher: RegExp
+  transform: () => string
+}
+
+/**
+ * @beta
+ */
+export function defineTextTransformRule(config: TextTransformRule): InputRule {
+  return {
+    matcher: config.matcher,
+    transform: ({snapshot, event}) => {
+      const matches = event.matches.flatMap((match) =>
+        match.groupMatches.length === 0 ? [match] : match.groupMatches,
+      )
+      const textLengthDelta = matches.reduce((length, match) => {
+        return (
+          length -
+          (config.transform().length -
+            (match.targetOffsets.focus.offset -
+              match.targetOffsets.anchor.offset))
+        )
+      }, 0)
+
+      const newText = event.textBefore + event.textInserted
+      const endCaretPosition = {
+        path: event.focusTextBlock.path,
+        offset: newText.length - textLengthDelta,
+      }
+
+      const actions = matches.reverse().flatMap((match) => [
+        raise({type: 'select', at: match.targetOffsets}),
+        raise({type: 'delete', at: match.targetOffsets}),
+        raise({
+          type: 'insert.child',
+          child: {
+            _type: snapshot.context.schema.span.name,
+            text: config.transform(),
+            marks:
+              getMarkState({
+                ...snapshot,
+                context: {
+                  ...snapshot.context,
+                  selection: {
+                    anchor: match.selection.anchor,
+                    focus: {
+                      path: match.selection.focus.path,
+                      offset: Math.min(
+                        match.selection.focus.offset,
+                        event.textBefore.length,
+                      ),
+                    },
+                  },
+                },
+              })?.marks ?? [],
+          },
+        }),
+      ])
+
+      return {
+        actions: [
+          ...actions,
+          raise({
+            type: 'select',
+            at: {
+              anchor: endCaretPosition,
+              focus: endCaretPosition,
+            },
+          }),
+        ],
+      }
+    },
+  }
+}
