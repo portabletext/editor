6d9fb9d950ba88d495c3de0198e621b7803a7f71
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-10-31 11:59:02 +0100

    fix(`isSelectionExpanded`): return `false` if there's no selection

---
+++ .changeset/nine-days-shave.md
@@ -0,0 +1,5 @@
+---
+'@portabletext/editor': patch
+---
+
+fix(`isSelectionExpanded`): return `false` if there's no selection
--- packages/editor/src/selectors/selector.is-selection-expanded.ts
+++ packages/editor/src/selectors/selector.is-selection-expanded.ts
@@ -5,5 +5,5 @@ import {isSelectionCollapsed} from './selector.is-selection-collapsed'
  * @public
  */
 export const isSelectionExpanded: EditorSelector<boolean> = (snapshot) => {
-  return !isSelectionCollapsed(snapshot)
+  return snapshot.context.selection !== null && !isSelectionCollapsed(snapshot)
 }
