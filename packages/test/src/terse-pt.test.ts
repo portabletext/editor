6e763b8c4e4b664c1d7b504a6b40f39a9ab57d68
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-11-05 10:17:30 +0100

    fix: make terse PT utils adhere to the schema

--- packages/test/src/terse-pt.test.ts
+++ packages/test/src/terse-pt.test.ts
@@ -5,112 +5,165 @@ import {createTestKeyGenerator} from './test-key-generator'
 
 const keyGenerator = createTestKeyGenerator()
 
-test(getTersePt.name, () => {
-  const schema = compileSchema(defineSchema({}))
-  const fooBlock = {
-    _key: 'b1',
-    _type: 'block',
-    children: [{_key: 's1', _type: 'span', text: 'foo'}],
-  }
-  const emptyBlock = {
-    _key: 'b2',
-    _type: 'block',
-    children: [{_key: 's2', _type: 'span', text: ''}],
-  }
-  const barBlock = {
-    _key: 'b3',
-    _type: 'block',
-    children: [{_key: 's3', _type: 'span', text: 'bar'}],
-  }
-  const softReturnBlock = {
-    _key: 'b4',
-    _type: 'block',
-    children: [{_key: 's4', _type: 'span', text: 'foo\nbar'}],
-  }
+describe(getTersePt.name, () => {
+  test('basic cases', () => {
+    const schema = compileSchema(defineSchema({}))
+    const fooBlock = {
+      _key: 'b1',
+      _type: 'block',
+      children: [{_key: 's1', _type: 'span', text: 'foo'}],
+    }
+    const emptyBlock = {
+      _key: 'b2',
+      _type: 'block',
+      children: [{_key: 's2', _type: 'span', text: ''}],
+    }
+    const barBlock = {
+      _key: 'b3',
+      _type: 'block',
+      children: [{_key: 's3', _type: 'span', text: 'bar'}],
+    }
+    const softReturnBlock = {
+      _key: 'b4',
+      _type: 'block',
+      children: [{_key: 's4', _type: 'span', text: 'foo\nbar'}],
+    }
 
-  expect(getTersePt({schema, value: [fooBlock, barBlock]})).toEqual([
-    'foo',
-    'bar',
-  ])
-  expect(getTersePt({schema, value: [emptyBlock, barBlock]})).toEqual([
-    '',
-    'bar',
-  ])
-  expect(getTersePt({schema, value: [fooBlock, emptyBlock, barBlock]})).toEqual(
-    ['foo', '', 'bar'],
-  )
-  expect(getTersePt({schema, value: [fooBlock, softReturnBlock]})).toEqual([
-    'foo',
-    'foo\nbar',
-  ])
+    expect(getTersePt({schema, value: [fooBlock, barBlock]})).toEqual([
+      'foo',
+      'bar',
+    ])
+    expect(getTersePt({schema, value: [emptyBlock, barBlock]})).toEqual([
+      '',
+      'bar',
+    ])
+    expect(
+      getTersePt({schema, value: [fooBlock, emptyBlock, barBlock]}),
+    ).toEqual(['foo', '', 'bar'])
+    expect(getTersePt({schema, value: [fooBlock, softReturnBlock]})).toEqual([
+      'foo',
+      'foo\nbar',
+    ])
 
-  expect(
-    getTersePt({
-      schema,
-      value: [
+    expect(
+      getTersePt({
+        schema,
+        value: [
+          {
+            _key: keyGenerator(),
+            _type: 'block',
+            children: [{_key: keyGenerator(), _type: 'span', text: 'foo'}],
+          },
+        ],
+      }),
+    ).toEqual(['foo'])
+    expect(
+      getTersePt({
+        schema,
+        value: [
+          {
+            _key: keyGenerator(),
+            _type: 'block',
+            children: [{_key: keyGenerator(), _type: 'span', text: 'foo'}],
+            listItem: 'number',
+          },
+        ],
+      }),
+    ).toEqual(['#:foo'])
+    expect(
+      getTersePt({
+        schema,
+        value: [
+          {
+            _key: keyGenerator(),
+            _type: 'block',
+            children: [{_key: keyGenerator(), _type: 'span', text: 'foo'}],
+            listItem: 'number',
+            style: 'h3',
+          },
+        ],
+      }),
+    ).toEqual(['#h3:foo'])
+    expect(
+      getTersePt({
+        schema,
+        value: [
+          {
+            _key: keyGenerator(),
+            _type: 'block',
+            children: [{_key: keyGenerator(), _type: 'span', text: 'foo'}],
+            level: 2,
+            listItem: 'number',
+            style: 'h3',
+          },
+        ],
+      }),
+    ).toEqual(['>>#h3:foo'])
+    expect(
+      getTersePt({
+        schema,
+        value: [
+          {
+            _key: keyGenerator(),
+            _type: 'block',
+            children: [{_key: keyGenerator(), _type: 'span', text: 'foo'}],
+            style: 'h3',
+          },
+        ],
+      }),
+    ).toEqual(['h3:foo'])
+  })
+
+  describe('extended example', () => {
+    const tersePt = [
+      'h1:Hello world!',
+      '{image}',
+      '>-:Here are some unordered list items',
+      '>-:With a nested ordered list:',
+      '>>#:Ordered list item A',
+      '>>#:Ordered list item B',
+      'And here is a paragraph with an inline ,{stock-ticker},',
+      'q:And a quote',
+    ]
+
+    test('no schema', () => {
+      const schema = compileSchema(defineSchema({}))
+      const blocks = parseTersePt(
         {
-          _key: keyGenerator(),
-          _type: 'block',
-          children: [{_key: keyGenerator(), _type: 'span', text: 'foo'}],
+          schema,
+          keyGenerator: createTestKeyGenerator(),
         },
-      ],
-    }),
-  ).toEqual(['foo'])
-  expect(
-    getTersePt({
-      schema,
-      value: [
-        {
-          _key: keyGenerator(),
-          _type: 'block',
-          children: [{_key: keyGenerator(), _type: 'span', text: 'foo'}],
-          listItem: 'number',
-        },
-      ],
-    }),
-  ).toEqual(['#:foo'])
-  expect(
-    getTersePt({
-      schema,
-      value: [
-        {
-          _key: keyGenerator(),
-          _type: 'block',
-          children: [{_key: keyGenerator(), _type: 'span', text: 'foo'}],
-          listItem: 'number',
-          style: 'h3',
-        },
-      ],
-    }),
-  ).toEqual(['#h3:foo'])
-  expect(
-    getTersePt({
-      schema,
-      value: [
-        {
-          _key: keyGenerator(),
-          _type: 'block',
-          children: [{_key: keyGenerator(), _type: 'span', text: 'foo'}],
-          level: 2,
-          listItem: 'number',
-          style: 'h3',
-        },
-      ],
-    }),
-  ).toEqual(['>>#h3:foo'])
-  expect(
-    getTersePt({
-      schema,
-      value: [
-        {
-          _key: keyGenerator(),
-          _type: 'block',
-          children: [{_key: keyGenerator(), _type: 'span', text: 'foo'}],
-          style: 'h3',
-        },
-      ],
-    }),
-  ).toEqual(['h3:foo'])
+        tersePt,
+      )
+
+      expect(getTersePt({schema, value: blocks})).toEqual([
+        'Hello world!',
+        '>:Here are some unordered list items',
+        '>:With a nested ordered list:',
+        '>>:Ordered list item A',
+        '>>:Ordered list item B',
+        'And here is a paragraph with an inline ,',
+        'And a quote',
+      ])
+    })
+
+    test('with schema', () => {
+      const schema = compileSchema(
+        defineSchema({
+          blockObjects: [{name: 'image'}],
+          inlineObjects: [{name: 'stock-ticker'}],
+          lists: [{name: 'bullet'}, {name: 'number'}],
+          styles: [{name: 'h1'}, {name: 'blockquote'}],
+        }),
+      )
+      const blocks = parseTersePt(
+        {schema, keyGenerator: createTestKeyGenerator()},
+        tersePt,
+      )
+
+      expect(getTersePt({schema, value: blocks})).toEqual(tersePt)
+    })
+  })
 })
 
 test(parseTersePtString.name, () => {
@@ -132,7 +185,11 @@ describe(parseTersePt.name, () => {
     expect(
       parseTersePt(
         {
-          schema: compileSchema(defineSchema({})),
+          schema: compileSchema(
+            defineSchema({
+              lists: [{name: 'bullet'}],
+            }),
+          ),
           keyGenerator: createTestKeyGenerator(),
         },
         parseTersePtString('-:'),
@@ -196,11 +253,33 @@ describe(parseTersePt.name, () => {
     ])
   })
 
-  test('colon in content', () => {
+  test('unknown style', () => {
     expect(
       parseTersePt(
         {
           schema: compileSchema(defineSchema({})),
+          keyGenerator: createTestKeyGenerator(),
+        },
+        parseTersePtString('h1:foo'),
+      ),
+    ).toEqual([
+      {
+        _key: 'k0',
+        _type: 'block',
+        children: [{_key: 'k1', _type: 'span', text: 'foo'}],
+      },
+    ])
+  })
+
+  test('colon in content', () => {
+    expect(
+      parseTersePt(
+        {
+          schema: compileSchema(
+            defineSchema({
+              styles: [{name: 'h1'}],
+            }),
+          ),
           keyGenerator: createTestKeyGenerator(),
         },
         parseTersePtString('h1:foo:bar'),
@@ -215,11 +294,39 @@ describe(parseTersePt.name, () => {
     ])
   })
 
-  test('extended example', () => {
+  test('unknown inline object', () => {
     expect(
       parseTersePt(
         {
           schema: compileSchema(defineSchema({})),
+          keyGenerator: createTestKeyGenerator(),
+        },
+        parseTersePtString('foo ,{stock-ticker}, bar'),
+      ),
+    ).toEqual([
+      {
+        _key: 'k0',
+        _type: 'block',
+        children: [
+          {_key: 'k1', _type: 'span', text: 'foo '},
+          {_key: 'k2', _type: 'span', text: ' bar'},
+        ],
+      },
+    ])
+  })
+
+  test('extended example', () => {
+    expect(
+      parseTersePt(
+        {
+          schema: compileSchema(
+            defineSchema({
+              blockObjects: [{name: 'image'}],
+              inlineObjects: [{name: 'stock-ticker'}],
+              lists: [{name: 'bullet'}, {name: 'number'}],
+              styles: [{name: 'h4'}],
+            }),
+          ),
           keyGenerator: createTestKeyGenerator(),
         },
         parseTersePtString('{image}|foo|>>#h4:bar|-:baz,fizz|,{stock-ticker},'),
@@ -260,6 +367,102 @@ describe(parseTersePt.name, () => {
           {_key: 'k10', _type: 'stock-ticker'},
           {_key: 'k11', _type: 'span', text: ''},
         ],
+      },
+    ])
+  })
+
+  test('extended example #2', () => {
+    const tersePt = [
+      'h1:Hello world!',
+      '{image}',
+      '>-:Here are some unordered list items',
+      '>-:With a nested ordered list:',
+      '>>#:Ordered list item A',
+      '>>#:Ordered list item B',
+      'And here is a paragraph with an inline ,{stock-ticker},',
+      'q:And a quote',
+    ]
+
+    expect(
+      parseTersePt(
+        {
+          schema: compileSchema(
+            defineSchema({
+              blockObjects: [{name: 'image'}],
+              inlineObjects: [{name: 'stock-ticker'}],
+              lists: [{name: 'bullet'}, {name: 'number'}],
+              styles: [{name: 'h1'}, {name: 'blockquote'}],
+            }),
+          ),
+          keyGenerator: createTestKeyGenerator(),
+        },
+        tersePt,
+      ),
+    ).toEqual([
+      {
+        _key: 'k0',
+        _type: 'block',
+        children: [{_key: 'k1', _type: 'span', text: 'Hello world!'}],
+        style: 'h1',
+      },
+      {
+        _key: 'k2',
+        _type: 'image',
+      },
+      {
+        _key: 'k3',
+        _type: 'block',
+        children: [
+          {
+            _key: 'k4',
+            _type: 'span',
+            text: 'Here are some unordered list items',
+          },
+        ],
+        listItem: 'bullet',
+        level: 1,
+      },
+      {
+        _key: 'k5',
+        _type: 'block',
+        children: [
+          {_key: 'k6', _type: 'span', text: 'With a nested ordered list:'},
+        ],
+        listItem: 'bullet',
+        level: 1,
+      },
+      {
+        _key: 'k7',
+        _type: 'block',
+        children: [{_key: 'k8', _type: 'span', text: 'Ordered list item A'}],
+        level: 2,
+        listItem: 'number',
+      },
+      {
+        _key: 'k9',
+        _type: 'block',
+        children: [{_key: 'k10', _type: 'span', text: 'Ordered list item B'}],
+        level: 2,
+        listItem: 'number',
+      },
+      {
+        _key: 'k11',
+        _type: 'block',
+        children: [
+          {
+            _key: 'k12',
+            _type: 'span',
+            text: 'And here is a paragraph with an inline ',
+          },
+          {_key: 'k13', _type: 'stock-ticker'},
+          {_key: 'k14', _type: 'span', text: ''},
+        ],
+      },
+      {
+        _key: 'k15',
+        _type: 'block',
+        children: [{_key: 'k16', _type: 'span', text: 'And a quote'}],
+        style: 'blockquote',
       },
     ])
   })
--- packages/test/src/terse-pt.ts
+++ packages/test/src/terse-pt.ts
@@ -6,22 +6,105 @@ import {
   type Schema,
 } from '@portabletext/schema'
 
-type TersePtConfig = {
-  style: (name?: string) => string
-  listItem: (name?: string) => string
-  level: (level?: number) => string
+type SerializeConfig = {
+  style?: (context: {schema: Schema; name: string | undefined}) => string
+  listItem?: (context: {schema: Schema; name: string | undefined}) => string
+  level?: (context: {schema: Schema; level: number | undefined}) => string
 }
 
-const defaultConfig: TersePtConfig = {
-  style: (name) =>
-    name === undefined || name === 'normal'
-      ? ''
-      : name === 'blockquote'
-        ? 'q'
-        : `${name}`,
-  listItem: (name) => (name === undefined ? '' : name === 'number' ? '#' : '-'),
-  level: (level) => (level === undefined ? '' : '>'.repeat(level)),
+const defaultSerializeConfig = {
+  style: (context) => {
+    const name =
+      context.name === undefined || context.name === 'normal'
+        ? ''
+        : context.name === 'blockquote'
+          ? 'q'
+          : `${context.name}`
+
+    return name
+  },
+  listItem: (context) => {
+    const name =
+      context.name === undefined
+        ? ''
+        : context.name === 'number'
+          ? '#'
+          : context.name === 'bullet'
+            ? '-'
+            : ''
+
+    return name
+  },
+  level: (context) => {
+    const level = context.level === undefined ? 0 : context.level
+
+    return level > 0 ? '>'.repeat(level) : ''
+  },
+} satisfies SerializeConfig
+
+type DeserializeConfig = {
+  blockObject?: (context: {schema: Schema; name: string}) =>
+    | {
+        _type: string
+      }
+    | undefined
+  inlineObject?: (context: {schema: Schema; name: string}) =>
+    | {
+        _type: string
+      }
+    | undefined
+  style?: (context: {
+    schema: Schema
+    symbol: string
+  }) => Schema['styles'][number]['name'] | undefined
+  listItem?: (context: {
+    schema: Schema
+    symbol: string
+  }) => Schema['lists'][number]['name'] | undefined
+  level?: (context: {schema: Schema; symbol: string}) => number | undefined
 }
+
+const defaultDeserializeConfig = {
+  blockObject: (context) => {
+    const type = context.schema.blockObjects.find(
+      (blockObject) => blockObject.name === context.name,
+    )?.name
+
+    return type ? {_type: type} : undefined
+  },
+  inlineObject: (context) => {
+    const type = context.schema.inlineObjects.find(
+      (inlineObject) => inlineObject.name === context.name,
+    )?.name
+
+    return type ? {_type: type} : undefined
+  },
+  style: (context) => {
+    const name = context.symbol === 'q' ? 'blockquote' : context.symbol
+
+    return (
+      context.schema.styles.find((style) => style.name === name)?.name ??
+      undefined
+    )
+  },
+  listItem: (context) => {
+    const name =
+      context.symbol === '#'
+        ? 'number'
+        : context.symbol === '-'
+          ? 'bullet'
+          : undefined
+
+    return (
+      context.schema.lists.find((list) => list.name === name)?.name ?? undefined
+    )
+  },
+  level: (context) => {
+    const level = context.symbol.split('').filter((part) => part === '>').length
+
+    return level > 0 ? level : undefined
+  },
+} satisfies DeserializeConfig
 
 type Context = {
   keyGenerator: () => string
@@ -38,7 +121,20 @@ export function getTersePt(
     let terseBlock = ''
 
     if (isTextBlock(context, block)) {
-      const blockPrefix = `${defaultConfig.level(block.level)}${defaultConfig.listItem(block.listItem)}${defaultConfig.style(block.style)}`
+      const blockPrefix = [
+        defaultSerializeConfig.level({
+          schema: context.schema,
+          level: block.level,
+        }),
+        defaultSerializeConfig.listItem({
+          schema: context.schema,
+          name: block.listItem,
+        }),
+        defaultSerializeConfig.style({
+          schema: context.schema,
+          name: block.style,
+        }),
+      ].join('')
 
       if (blockPrefix) {
         terseBlock = `${blockPrefix}:`
@@ -72,10 +168,17 @@ export function parseTersePt(
 
   for (const terseBlock of tersePt) {
     if (terseBlock.startsWith('{')) {
-      blocks.push({
-        _type: terseBlock.slice(1, -1),
-        _key: context.keyGenerator(),
+      const blockObject = defaultDeserializeConfig.blockObject({
+        schema: context.schema,
+        name: terseBlock.slice(1, -1),
       })
+
+      if (blockObject) {
+        blocks.push({
+          ...blockObject,
+          _key: context.keyGenerator(),
+        })
+      }
 
       continue
     }
@@ -90,18 +193,31 @@ export function parseTersePt(
       const [prefix = '', ...contentParts] = terseBlock.split(':')
       const content = contentParts.join(':')
 
-      const listItem = prefix.includes('#')
-        ? 'number'
+      const listItemSymbol = prefix.includes('#')
+        ? '#'
         : prefix.includes('-')
-          ? 'bullet'
+          ? '-'
           : undefined
+      const listItem = listItemSymbol
+        ? defaultDeserializeConfig.listItem({
+            schema: context.schema,
+            symbol: listItemSymbol,
+          })
+        : undefined
 
       if (listItem !== undefined) {
         block.listItem = listItem
       }
 
       const initialLevel = listItem !== undefined ? 1 : 0
-      const level = prefix.split('').filter((part) => part === '>').length
+      const level =
+        defaultDeserializeConfig.level({
+          schema: context.schema,
+          symbol: prefix
+            .split('')
+            .filter((part) => part === '>')
+            .join(''),
+        }) ?? 0
 
       if (level > 0) {
         block.level = level
@@ -109,24 +225,38 @@ export function parseTersePt(
         block.level = initialLevel
       }
 
-      const style = prefix
+      const styleSymbol = prefix
         .split('')
         .filter((part) => !['#', '-', '>'].includes(part))
         .join('')
+
+      const style = defaultDeserializeConfig.style({
+        schema: context.schema,
+        symbol: styleSymbol,
+      })
 
       if (style) {
         block.style = style
       }
 
       const textRuns =
-        !style && !listItem ? `:${content}`.split(',') : content.split(',')
+        !styleSymbol && !listItemSymbol
+          ? `:${content}`.split(',')
+          : content.split(',')
 
       for (const textRun of textRuns) {
         if (textRun.startsWith('{')) {
-          block.children.push({
-            _key: context.keyGenerator(),
-            _type: textRun.slice(1, -1),
+          const inlineObject = defaultDeserializeConfig.inlineObject({
+            schema: context.schema,
+            name: textRun.slice(1, -1),
           })
+
+          if (inlineObject) {
+            block.children.push({
+              ...inlineObject,
+              _key: context.keyGenerator(),
+            })
+          }
         } else {
           block.children.push({
             _key: context.keyGenerator(),
@@ -140,10 +270,17 @@ export function parseTersePt(
 
       for (const textRun of textRuns) {
         if (textRun.startsWith('{')) {
-          block.children.push({
-            _key: context.keyGenerator(),
-            _type: textRun.slice(1, -1),
+          const inlineObject = defaultDeserializeConfig.inlineObject({
+            schema: context.schema,
+            name: textRun.slice(1, -1),
           })
+
+          if (inlineObject) {
+            block.children.push({
+              ...inlineObject,
+              _key: context.keyGenerator(),
+            })
+          }
         } else {
           block.children.push({
             _key: context.keyGenerator(),
