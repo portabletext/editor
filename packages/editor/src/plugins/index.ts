d2b53c099d07a2b4d411c8bc87f1cf02415e5194
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-11-05 13:04:25 +0100

    feat!: remove deprecated plugins

--- packages/editor/src/plugins/index.ts
+++ packages/editor/src/plugins/index.ts
@@ -1,6 +1,3 @@
 export {BehaviorPlugin} from './plugin.behavior'
-export {DecoratorShortcutPlugin} from './plugin.decorator-shortcut'
 export {EditorRefPlugin} from './plugin.editor-ref'
 export {EventListenerPlugin} from './plugin.event-listener'
-export {MarkdownPlugin, type MarkdownPluginConfig} from './plugin.markdown'
-export {OneLinePlugin} from './plugin.one-line'
