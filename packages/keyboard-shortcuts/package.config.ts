032b07dd83e4f1f11446df8657de310fe36c5095
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-11-06 18:54:32 +0100

    fix(deps): remove react-compiler-runtime
    The package does not use or depend on React.

--- packages/keyboard-shortcuts/package.config.ts
+++ packages/keyboard-shortcuts/package.config.ts
@@ -2,7 +2,5 @@ import {defineConfig} from '@sanity/pkg-utils'
 
 export default defineConfig({
   tsconfig: 'tsconfig.dist.json',
-  babel: {reactCompiler: true},
-  reactCompilerOptions: {target: '18'},
   dts: 'rolldown',
 })
