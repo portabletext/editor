f2ab998821340df027cc6f1e1f537386eeecae05
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-11-10 13:27:11 +0100

    test: port removing-blocks tests to new test suite

--- packages/editor/gherkin-tests-v2/removing-blocks.test.ts
+++ packages/editor/gherkin-tests-v2/removing-blocks.test.ts
@@ -1,7 +1,7 @@
 import {Feature} from 'racejar/vitest'
 import removingBlocksFeature from '../gherkin-spec/removing-blocks.feature?raw'
-import {parameterTypes} from './gherkin-parameter-types'
-import {stepDefinitions} from './gherkin-step-definitions'
+import {parameterTypes} from '../src/test'
+import {stepDefinitions} from '../src/test/vitest'
 
 Feature({
   featureText: removingBlocksFeature,
