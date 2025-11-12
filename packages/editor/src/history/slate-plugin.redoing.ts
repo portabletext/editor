c2f77aa56b329cba5f0f26f2e44077866f6557cc
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-11-12 09:59:21 +0100

    refactor: rename `withRedoing`->`pluginRedoing`

--- packages/editor/src/history/slate-plugin.redoing.ts
+++ packages/editor/src/history/slate-plugin.redoing.ts
@@ -2,10 +2,13 @@ import type {Editor} from 'slate'
 
 const IS_REDOING: WeakMap<Editor, boolean | undefined> = new WeakMap()
 
-export function withRedoing(editor: Editor, fn: () => void) {
+export function pluginRedoing(editor: Editor, fn: () => void) {
   const prev = isRedoing(editor)
+
   IS_REDOING.set(editor, true)
+
   fn()
+
   IS_REDOING.set(editor, prev)
 }
 
