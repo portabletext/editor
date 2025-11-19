0594087606ff65a92f7c0fbcd9554c6d1917a2cf
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-11-19 10:43:58 +0100

    test: move `transform` to own file

---
+++ packages/block-tools/test/html-to-blocks/test-utils.ts
@@ -0,0 +1,92 @@
+import {
+  compileSchema,
+  defineSchema,
+  type SchemaDefinition,
+} from '@portabletext/schema'
+import {JSDOM} from 'jsdom'
+import {assert, expect} from 'vitest'
+import {htmlToBlocks, type ImageSchemaMatcher} from '../../src'
+import {createTestKeyGenerator} from '../test-key-generator'
+
+const imageMatcher: ImageSchemaMatcher = ({context, props}) => {
+  if (
+    !context.schema.blockObjects.some(
+      (blockObject) => blockObject.name === 'image',
+    )
+  ) {
+    return undefined
+  }
+
+  return {
+    _type: 'image',
+    ...(props.src ? {src: props.src} : {}),
+    ...(props.alt ? {alt: props.alt} : {}),
+  }
+}
+
+const inlineImageMatcher: ImageSchemaMatcher = ({context, props}) => {
+  if (
+    !context.schema.inlineObjects.some(
+      (inlineObject) => inlineObject.name === 'image',
+    )
+  ) {
+    return undefined
+  }
+
+  return {
+    _type: 'image',
+    ...(props.src ? {src: props.src} : {}),
+    ...(props.alt ? {alt: props.alt} : {}),
+  }
+}
+
+export function transform(
+  html: Array<string>,
+  schemaDefinition?: SchemaDefinition,
+) {
+  const defaultSchema = compileSchema(
+    defineSchema({
+      blockObjects: [
+        {
+          name: 'image',
+          fields: [{name: 'src', type: 'string'}],
+        },
+      ],
+      inlineObjects: [
+        {
+          name: 'image',
+          fields: [{name: 'src', type: 'string'}],
+        },
+      ],
+    }),
+  )
+
+  const blocksArray = html.map((html) => {
+    return htmlToBlocks(
+      html,
+      schemaDefinition ? compileSchema(schemaDefinition) : defaultSchema,
+      {
+        parseHtml: (html) => new JSDOM(html).window.document,
+        keyGenerator: createTestKeyGenerator('k'),
+        unstable_whitespaceOnPasteMode: 'normalize',
+        matchers: {
+          image: imageMatcher,
+          inlineImage: inlineImageMatcher,
+        },
+      },
+    )
+  })
+
+  if (blocksArray.length === 0) {
+    assert.fail('No blocks were returned')
+  }
+
+  for (let i = 1; i < blocksArray.length; i++) {
+    expect(
+      blocksArray[i],
+      `returned blocks for each html blob should be equal`,
+    ).toEqual(blocksArray[0])
+  }
+
+  return blocksArray[0]
+}
