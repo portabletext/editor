524e31a9c105f5b82e2c95cb846e9a9daef046af
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-12-09 14:12:08 +0100

    test: improve 'Scenario: Inserting and focusing inline object'

--- packages/editor/tests/event.insert.inline-object.test.tsx
+++ packages/editor/tests/event.insert.inline-object.test.tsx
@@ -6,6 +6,7 @@ import {page, userEvent} from 'vitest/browser'
 import type {EditorEmittedEvent} from '../src'
 import {EventListenerPlugin} from '../src/plugins/plugin.event-listener'
 import {createTestEditor} from '../src/test/vitest'
+import {isKeyedSegment} from '../src/utils'
 
 describe('event.insert.inline object', () => {
   test('Scenario: Inserting inline object without any initial fields', async () => {
@@ -51,6 +52,16 @@ describe('event.insert.inline object', () => {
 
   test('Scenario: Inserting and focusing inline object', async () => {
     const focusEvents: Array<EditorEmittedEvent> = []
+
+    let resolveInitialSelection: () => void
+    const initialSelectionPromise = new Promise<void>((resolve) => {
+      resolveInitialSelection = resolve
+    })
+
+    let resolveInlineObjectSelection: () => void
+    const inlineObjectSelectionPromise = new Promise<void>((resolve) => {
+      resolveInlineObjectSelection = resolve
+    })
     const {editor, locator} = await createTestEditor({
       children: (
         <>
@@ -79,6 +90,26 @@ describe('event.insert.inline object', () => {
               if (event.type === 'focused') {
                 focusEvents.push(event)
               }
+
+              if (event.type === 'selection') {
+                const childSegment = event.selection?.focus.path.at(2)
+
+                if (
+                  childSegment &&
+                  isKeyedSegment(childSegment) &&
+                  childSegment._key === 'k1'
+                ) {
+                  resolveInitialSelection()
+                }
+
+                if (
+                  childSegment &&
+                  isKeyedSegment(childSegment) &&
+                  childSegment._key === 'k2'
+                ) {
+                  resolveInlineObjectSelection()
+                }
+              }
             }}
           />
         </>
@@ -100,14 +131,16 @@ describe('event.insert.inline object', () => {
 
     await userEvent.click(locator)
 
-    // Focusing in Slate is async, so we need to make sure it has settled
     await vi.waitFor(() => {
       expect(focusEvents).toHaveLength(1)
     })
 
+    await initialSelectionPromise
+
     await userEvent.click(insertStockTickerButton)
 
-    // Focusing in Slate is async, so we need to make sure it has settled
+    await inlineObjectSelectionPromise
+
     await vi.waitFor(() => {
       expect(focusEvents).toHaveLength(2)
     })
@@ -127,6 +160,7 @@ describe('event.insert.inline object', () => {
         },
       ])
     })
+
     await vi.waitFor(() => {
       expect(editor.getSnapshot().context.selection).toEqual({
         anchor: {
@@ -142,6 +176,21 @@ describe('event.insert.inline object', () => {
     })
 
     await userEvent.keyboard('{ArrowRight}')
+
+    await vi.waitFor(() => {
+      expect(editor.getSnapshot().context.selection).toEqual({
+        anchor: {
+          path: [{_key: 'k0'}, 'children', {_key: 'k3'}],
+          offset: 0,
+        },
+        focus: {
+          path: [{_key: 'k0'}, 'children', {_key: 'k3'}],
+          offset: 0,
+        },
+        backward: false,
+      })
+    })
+
     await userEvent.type(locator, 'foo')
 
     await vi.waitFor(() => {
@@ -158,20 +207,6 @@ describe('event.insert.inline object', () => {
           style: 'normal',
         },
       ])
-    })
-
-    await vi.waitFor(() => {
-      expect(editor.getSnapshot().context.selection).toEqual({
-        anchor: {
-          path: [{_key: 'k0'}, 'children', {_key: 'k3'}],
-          offset: 3,
-        },
-        focus: {
-          path: [{_key: 'k0'}, 'children', {_key: 'k3'}],
-          offset: 3,
-        },
-        backward: false,
-      })
     })
   })
 
