17839a6e6fecfd654cda334fec3ecf30e14fd69f
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-12-12 10:51:12 +0100

    refactor: move `decoratorRemovOperationImplementation` to its own file

--- packages/editor/src/editor/plugins/createWithPortableTextMarkModel.ts
+++ packages/editor/src/editor/plugins/createWithPortableTextMarkModel.ts
@@ -5,11 +5,10 @@
  */
 
 import type {PortableTextObject, PortableTextSpan} from '@portabletext/schema'
-import {Editor, Element, Node, Path, Range, Text, Transforms} from 'slate'
+import {Editor, Node, Path, Range, Text, Transforms} from 'slate'
 import {createPlaceholderBlock} from '../../internal-utils/create-placeholder-block'
 import {debugWithName} from '../../internal-utils/debug'
 import {isEqualMarkDefs} from '../../internal-utils/equality'
-import type {BehaviorOperationImplementation} from '../../operations/behavior.operations'
 import type {PortableTextSlateEditor} from '../../types/slate-editor'
 import type {EditorActor} from '../editor-machine'
 import {withNormalizeNode} from '../with-normalizing-node'
@@ -401,84 +400,5 @@ export function createWithPortableTextMarkModel(
     }
 
     return editor
-  }
-}
-
-export const removeDecoratorOperationImplementation: BehaviorOperationImplementation<
-  'decorator.remove'
-> = ({operation}) => {
-  const editor = operation.editor
-  const mark = operation.decorator
-  const {selection} = editor
-
-  if (selection) {
-    if (Range.isExpanded(selection)) {
-      // Split if needed
-      Transforms.setNodes(
-        editor,
-        {},
-        {match: Text.isText, split: true, hanging: true},
-      )
-      if (editor.selection) {
-        const splitTextNodes = [
-          ...Editor.nodes(editor, {
-            at: editor.selection,
-            match: Text.isText,
-          }),
-        ]
-        splitTextNodes.forEach(([node, path]) => {
-          const block = editor.children[path[0]]
-          if (Element.isElement(block) && block.children.includes(node)) {
-            Transforms.setNodes(
-              editor,
-              {
-                marks: (Array.isArray(node.marks) ? node.marks : []).filter(
-                  (eMark: string) => eMark !== mark,
-                ),
-                _type: 'span',
-              },
-              {at: path},
-            )
-          }
-        })
-      }
-    } else {
-      const [block, blockPath] = Editor.node(editor, selection, {
-        depth: 1,
-      })
-      const lonelyEmptySpan =
-        editor.isTextBlock(block) &&
-        block.children.length === 1 &&
-        editor.isTextSpan(block.children[0]) &&
-        block.children[0].text === ''
-          ? block.children[0]
-          : undefined
-
-      if (lonelyEmptySpan) {
-        const existingMarks = lonelyEmptySpan.marks ?? []
-        const existingMarksWithoutDecorator = existingMarks.filter(
-          (existingMark) => existingMark !== mark,
-        )
-
-        Transforms.setNodes(
-          editor,
-          {
-            marks: existingMarksWithoutDecorator,
-          },
-          {
-            at: blockPath,
-            match: (node) => editor.isTextSpan(node),
-          },
-        )
-      } else {
-        editor.decoratorState[mark] = false
-      }
-    }
-
-    if (editor.selection) {
-      // Reselect
-      const selection = editor.selection
-      editor.selection = {...selection}
-    }
   }
 }
