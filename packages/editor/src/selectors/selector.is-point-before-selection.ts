69c9a88e81bbc13f1ef9c384d3d8feaa9d9c3b2d
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-12-18 18:11:22 +0100

    refactor: abstract common `comparePoints` util

--- packages/editor/src/selectors/selector.is-point-before-selection.ts
+++ packages/editor/src/selectors/selector.is-point-before-selection.ts
@@ -1,11 +1,7 @@
-import {isTextBlock} from '@portabletext/schema'
 import type {EditorSelector} from '../editor/editor-selector'
 import type {EditorSelectionPoint} from '../types/editor'
+import {comparePoints} from '../utils/util.compare-points'
 import {getSelectionStartPoint} from '../utils/util.get-selection-start-point'
-import {
-  getBlockKeyFromSelectionPoint,
-  getChildKeyFromSelectionPoint,
-} from '../utils/util.selection-point'
 
 /**
  * @public
@@ -19,79 +15,7 @@ export function isPointBeforeSelection(
     }
 
     const startPoint = getSelectionStartPoint(snapshot.context.selection)
-    const startBlockKey = getBlockKeyFromSelectionPoint(startPoint)
-    const startChildKey = getChildKeyFromSelectionPoint(startPoint)
 
-    const pointBlockKey = getBlockKeyFromSelectionPoint(point)
-    const pointChildKey = getChildKeyFromSelectionPoint(point)
-
-    if (!pointBlockKey || !startBlockKey) {
-      return false
-    }
-
-    const startBlockIndex = snapshot.blockIndexMap.get(startBlockKey)
-    const pointBlockIndex = snapshot.blockIndexMap.get(pointBlockKey)
-
-    if (startBlockIndex === undefined || pointBlockIndex === undefined) {
-      return false
-    }
-
-    if (pointBlockIndex < startBlockIndex) {
-      // The point block is before the start block.
-      return true
-    }
-
-    if (pointBlockIndex > startBlockIndex) {
-      // The point block is after the start block.
-      return false
-    }
-
-    // The point block is the same as the start block.
-    const pointBlock = snapshot.context.value.at(pointBlockIndex)
-
-    if (!pointBlock) {
-      // The point block is not in the value.
-      return false
-    }
-
-    if (!isTextBlock(snapshot.context, pointBlock)) {
-      // The point block is not a text block.
-      // Since the point block is the same as the start block, the point is not
-      // before the selection.
-      return false
-    }
-
-    let pointChildIndex: number | undefined
-    let startChildIndex: number | undefined
-
-    let childIndex = -1
-
-    // The point block is the same as the start block, so we need to find the
-    // child indices and compare them.
-    for (const child of pointBlock.children) {
-      childIndex++
-
-      if (child._key === pointChildKey && child._key === startChildKey) {
-        return point.offset < startPoint.offset
-      }
-
-      if (child._key === pointChildKey) {
-        pointChildIndex = childIndex
-      }
-
-      if (child._key === startChildKey) {
-        startChildIndex = childIndex
-      }
-
-      if (pointChildIndex !== undefined && startChildIndex !== undefined) {
-        break
-      }
-    }
-
-    if (pointChildIndex === undefined || startChildIndex === undefined) {
-      return false
-    }
-
-    return pointChildIndex < startChildIndex
+    return comparePoints(snapshot, point, startPoint) === -1
   }
 }
