3920ad4230012881b5fdc1f3e16d0089e431fac7
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2025-12-19 09:42:12 +0100

    refactor: abstract common `getSelectedChildren` from `getSelectedSpans`

--- packages/editor/src/selectors/selector.get-selected-spans.ts
+++ packages/editor/src/selectors/selector.get-selected-spans.ts
@@ -1,12 +1,7 @@
-import {isSpan, isTextBlock, type PortableTextSpan} from '@portabletext/schema'
+import {isSpan, type PortableTextSpan} from '@portabletext/schema'
 import type {EditorSelector} from '../editor/editor-selector'
 import type {ChildPath} from '../types/paths'
-import {
-  getBlockKeyFromSelectionPoint,
-  getChildKeyFromSelectionPoint,
-} from '../utils/util.selection-point'
-import {getSelectionEndPoint} from './selector.get-selection-end-point'
-import {getSelectionStartPoint} from './selector.get-selection-start-point'
+import {getSelectedChildren} from './selector.get-selected-children'
 
 /**
  * @public
@@ -21,134 +16,7 @@ export const getSelectedSpans: EditorSelector<
     return []
   }
 
-  const selectedSpans: Array<{
-    node: PortableTextSpan
-    path: ChildPath
-  }> = []
-
-  const startPoint = getSelectionStartPoint(snapshot)
-  const endPoint = getSelectionEndPoint(snapshot)
-
-  if (!startPoint || !endPoint) {
-    return selectedSpans
-  }
-
-  const startBlockKey = getBlockKeyFromSelectionPoint(startPoint)
-  const endBlockKey = getBlockKeyFromSelectionPoint(endPoint)
-  const startSpanKey = getChildKeyFromSelectionPoint(startPoint)
-  const endSpanKey = getChildKeyFromSelectionPoint(endPoint)
-
-  if (!startBlockKey || !endBlockKey) {
-    return selectedSpans
-  }
-
-  const startBlockIndex = snapshot.blockIndexMap.get(startBlockKey)
-  const endBlockIndex = snapshot.blockIndexMap.get(endBlockKey)
-
-  if (startBlockIndex === undefined || endBlockIndex === undefined) {
-    return selectedSpans
-  }
-
-  const slicedValue = snapshot.context.value.slice(
-    startBlockIndex,
-    endBlockIndex + 1,
-  )
-
-  let startBlockFound = false
-
-  for (const block of slicedValue) {
-    if (block._key === startBlockKey) {
-      startBlockFound = true
-    }
-
-    if (!isTextBlock(snapshot.context, block)) {
-      continue
-    }
-
-    if (block._key === startBlockKey) {
-      for (const child of block.children) {
-        if (!isSpan(snapshot.context, child)) {
-          continue
-        }
-
-        if (startSpanKey && child._key === startSpanKey) {
-          if (startPoint.offset < child.text.length) {
-            selectedSpans.push({
-              node: child,
-              path: [{_key: block._key}, 'children', {_key: child._key}],
-            })
-          }
-
-          if (startSpanKey === endSpanKey) {
-            break
-          }
-
-          continue
-        }
-
-        if (endSpanKey && child._key === endSpanKey) {
-          if (endPoint.offset > 0) {
-            selectedSpans.push({
-              node: child,
-              path: [{_key: block._key}, 'children', {_key: child._key}],
-            })
-          }
-          break
-        }
-
-        if (selectedSpans.length > 0) {
-          selectedSpans.push({
-            node: child,
-            path: [{_key: block._key}, 'children', {_key: child._key}],
-          })
-        }
-      }
-
-      if (startBlockKey === endBlockKey) {
-        break
-      }
-
-      continue
-    }
-
-    if (block._key === endBlockKey) {
-      for (const child of block.children) {
-        if (!isSpan(snapshot.context, child)) {
-          continue
-        }
-
-        if (endSpanKey && child._key === endSpanKey) {
-          if (endPoint.offset > 0) {
-            selectedSpans.push({
-              node: child,
-              path: [{_key: block._key}, 'children', {_key: child._key}],
-            })
-          }
-          break
-        }
-
-        selectedSpans.push({
-          node: child,
-          path: [{_key: block._key}, 'children', {_key: child._key}],
-        })
-      }
-
-      break
-    }
-
-    if (startBlockFound) {
-      for (const child of block.children) {
-        if (!isSpan(snapshot.context, child)) {
-          continue
-        }
-
-        selectedSpans.push({
-          node: child,
-          path: [{_key: block._key}, 'children', {_key: child._key}],
-        })
-      }
-    }
-  }
-
-  return selectedSpans
+  return getSelectedChildren({
+    filter: (child) => isSpan(snapshot.context, child),
+  })(snapshot)
 }
