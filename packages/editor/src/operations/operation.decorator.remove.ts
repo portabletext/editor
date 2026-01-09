92e373402f8042f8f00eb97fff6225e670ccc502
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2026-01-09 16:02:39 +0100

    refactor: simplify `decorator.remove` operation with early return

--- packages/editor/src/operations/operation.decorator.remove.ts
+++ packages/editor/src/operations/operation.decorator.remove.ts
@@ -18,78 +18,81 @@ export const decoratorRemoveOperationImplementation: OperationImplementation<
       })
     : editor.selection
 
-  if (at) {
-    if (Range.isExpanded(at)) {
-      const rangeRef = Editor.rangeRef(editor, at, {affinity: 'inward'})
+  if (!at) {
+    // Unable to remove decorator without a selection
+    return
+  }
+
+  if (Range.isExpanded(at)) {
+    const rangeRef = Editor.rangeRef(editor, at, {affinity: 'inward'})
+
+    Transforms.setNodes(
+      editor,
+      {},
+      {at, match: Text.isText, split: true, hanging: true},
+    )
+
+    const updatedAt = rangeRef.unref()
+
+    if (updatedAt) {
+      const splitTextNodes = [
+        ...Editor.nodes(editor, {
+          at: updatedAt,
+          match: Text.isText,
+        }),
+      ]
+      splitTextNodes.forEach(([node, path]) => {
+        const block = editor.children[path[0]]
+        if (Element.isElement(block) && block.children.includes(node)) {
+          Transforms.setNodes(
+            editor,
+            {
+              marks: (Array.isArray(node.marks) ? node.marks : []).filter(
+                (eMark: string) => eMark !== mark,
+              ),
+              _type: 'span',
+            },
+            {at: path},
+          )
+        }
+      })
+    }
+  } else {
+    const [block, blockPath] = Editor.node(editor, at, {
+      depth: 1,
+    })
+    const lonelyEmptySpan =
+      editor.isTextBlock(block) &&
+      block.children.length === 1 &&
+      editor.isTextSpan(block.children[0]) &&
+      block.children[0].text === ''
+        ? block.children[0]
+        : undefined
+
+    if (lonelyEmptySpan) {
+      const existingMarks = lonelyEmptySpan.marks ?? []
+      const existingMarksWithoutDecorator = existingMarks.filter(
+        (existingMark) => existingMark !== mark,
+      )
 
       Transforms.setNodes(
         editor,
-        {},
-        {at, match: Text.isText, split: true, hanging: true},
+        {
+          marks: existingMarksWithoutDecorator,
+        },
+        {
+          at: blockPath,
+          match: (node) => editor.isTextSpan(node),
+        },
       )
-
-      const updatedAt = rangeRef.unref()
-
-      if (updatedAt) {
-        const splitTextNodes = [
-          ...Editor.nodes(editor, {
-            at: updatedAt,
-            match: Text.isText,
-          }),
-        ]
-        splitTextNodes.forEach(([node, path]) => {
-          const block = editor.children[path[0]]
-          if (Element.isElement(block) && block.children.includes(node)) {
-            Transforms.setNodes(
-              editor,
-              {
-                marks: (Array.isArray(node.marks) ? node.marks : []).filter(
-                  (eMark: string) => eMark !== mark,
-                ),
-                _type: 'span',
-              },
-              {at: path},
-            )
-          }
-        })
-      }
     } else {
-      const [block, blockPath] = Editor.node(editor, at, {
-        depth: 1,
-      })
-      const lonelyEmptySpan =
-        editor.isTextBlock(block) &&
-        block.children.length === 1 &&
-        editor.isTextSpan(block.children[0]) &&
-        block.children[0].text === ''
-          ? block.children[0]
-          : undefined
-
-      if (lonelyEmptySpan) {
-        const existingMarks = lonelyEmptySpan.marks ?? []
-        const existingMarksWithoutDecorator = existingMarks.filter(
-          (existingMark) => existingMark !== mark,
-        )
-
-        Transforms.setNodes(
-          editor,
-          {
-            marks: existingMarksWithoutDecorator,
-          },
-          {
-            at: blockPath,
-            match: (node) => editor.isTextSpan(node),
-          },
-        )
-      } else {
-        editor.decoratorState[mark] = false
-      }
+      editor.decoratorState[mark] = false
     }
+  }
 
-    if (editor.selection) {
-      // Reselect
-      const selection = editor.selection
-      editor.selection = {...selection}
-    }
+  if (editor.selection) {
+    // Reselect
+    const selection = editor.selection
+    editor.selection = {...selection}
   }
 }
