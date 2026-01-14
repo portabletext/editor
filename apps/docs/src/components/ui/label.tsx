444b9cdb9f05b75e5fe571c5adaf4a570f9715d3
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2026-01-14 14:45:24 +0100

    docs: improve landing page editor

---
+++ apps/docs/src/components/ui/label.tsx
@@ -0,0 +1,19 @@
+import {cn} from '@/lib/utils'
+import * as React from 'react'
+
+const Label = React.forwardRef<
+  HTMLLabelElement,
+  React.ComponentPropsWithoutRef<'label'>
+>(({className, ...props}, ref) => (
+  <label
+    ref={ref}
+    className={cn(
+      'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+      className,
+    )}
+    {...props}
+  />
+))
+Label.displayName = 'Label'
+
+export {Label}
