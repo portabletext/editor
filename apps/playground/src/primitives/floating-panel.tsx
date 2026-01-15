16232a9b90be94a9386e306aa6c024f7cfe3b07a
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2026-01-15 19:10:56 +0100

    feat: improve playground emoji picker
    - Fuzzy search with Fuse.js with a cached Fuse instance on each init
    - Proper floating UI with ... Floating UI
    - Slightly polished design

---
+++ apps/playground/src/primitives/floating-panel.tsx
@@ -0,0 +1,56 @@
+import {
+  autoUpdate,
+  flip,
+  offset,
+  shift,
+  useFloating,
+  type ReferenceType,
+} from '@floating-ui/react-dom'
+import type {ReactNode} from 'react'
+import {createPortal} from 'react-dom'
+import {tv} from 'tailwind-variants'
+
+interface FloatingPanelProps {
+  getAnchorRect: () => DOMRect | null
+  children: ReactNode
+  offset?: number
+}
+
+const styles = tv({
+  base: 'bg-white z-50 shadow-2xl rounded-lg overflow-hidden bg-clip-padding border border-black/10 text-slate-700',
+})
+
+export function FloatingPanel({
+  getAnchorRect,
+  children,
+  offset: offsetValue = 4,
+}: FloatingPanelProps) {
+  const {floatingStyles, refs} = useFloating({
+    placement: 'bottom-start',
+    middleware: [
+      offset(offsetValue),
+      flip({fallbackPlacements: ['top-start']}),
+      shift({padding: 8}),
+    ],
+    whileElementsMounted: autoUpdate,
+    elements: {
+      reference: createVirtualElement(getAnchorRect),
+    },
+  })
+  const {setFloating} = refs
+
+  return createPortal(
+    <div ref={setFloating} className={styles()} style={floatingStyles}>
+      {children}
+    </div>,
+    document.body,
+  )
+}
+
+function createVirtualElement(
+  getRect: () => DOMRect | null,
+): ReferenceType | null {
+  return {
+    getBoundingClientRect: () => getRect() ?? new DOMRect(),
+  }
+}
