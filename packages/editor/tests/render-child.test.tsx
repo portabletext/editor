c4a5b0e7afb2a3db702c8521db41cf0a04a2bb10
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2026-02-09 12:31:32 +0100

    test: failing test for `focused`/`selected` state for children with duplicate keys

--- packages/editor/tests/render-child.test.tsx
+++ packages/editor/tests/render-child.test.tsx
@@ -3,6 +3,7 @@ import {describe, expect, test, vi} from 'vitest'
 import {userEvent} from 'vitest/browser'
 import {defineSchema, type BlockChildRenderProps} from '../src'
 import {createTestEditor} from '../src/test/vitest'
+import type {Path} from '../src/types/paths'
 
 describe('renderChild', () => {
   test('span focused and selected props', async () => {
@@ -190,5 +191,192 @@ describe('renderChild', () => {
         selected: true,
       },
     ])
+  })
+
+  test('children with same keys across blocks', async () => {
+    const keyGenerator = createTestKeyGenerator()
+
+    // k0
+    const blockAKey = keyGenerator()
+    // k1
+    const blockBKey = keyGenerator()
+    // k2
+    const fooSpanKey = keyGenerator()
+    // k3
+    const stockTickerKey = keyGenerator()
+    // k4
+    const barSpanKey = keyGenerator()
+
+    let renderChildValues: Array<
+      Pick<BlockChildRenderProps, 'focused' | 'selected'> & {
+        path: Path
+      }
+    > = []
+
+    const renderChild = (props: BlockChildRenderProps) => {
+      renderChildValues.push({
+        focused: props.focused,
+        selected: props.selected,
+        path: props.path,
+      })
+      return props.children
+    }
+
+    const {editor, locator} = await createTestEditor({
+      keyGenerator,
+      initialValue: [
+        {
+          _type: 'block',
+          _key: blockAKey,
+          children: [
+            {_type: 'span', _key: fooSpanKey, text: 'foo', marks: []},
+            {_type: 'stock-ticker', _key: stockTickerKey, symbol: 'AAPL'},
+            {_type: 'span', _key: barSpanKey, text: 'bar', marks: []},
+          ],
+          markDefs: [],
+          style: 'normal',
+        },
+        {
+          _type: 'block',
+          _key: blockBKey,
+          children: [
+            {_type: 'span', _key: fooSpanKey, text: 'fizz', marks: []},
+            {_type: 'stock-ticker', _key: stockTickerKey, symbol: 'AAPL'},
+            {_type: 'span', _key: barSpanKey, text: 'buzz', marks: []},
+          ],
+          markDefs: [],
+          style: 'normal',
+        },
+      ],
+      schemaDefinition: defineSchema({
+        inlineObjects: [
+          {name: 'stock-ticker', fields: [{name: 'symbol', type: 'string'}]},
+        ],
+      }),
+      editableProps: {renderChild},
+    })
+
+    await vi.waitFor(() => {
+      expect(
+        renderChildValues,
+        'Unexpected renderChild calls after setup',
+      ).toEqual([
+        // Placeholder block
+        {
+          focused: false,
+          selected: false,
+          path: [{_key: 'k5'}, 'children', {_key: 'k6'}],
+        },
+        // Initial setup
+        {
+          focused: false,
+          selected: false,
+          path: [{_key: blockAKey}, 'children', {_key: fooSpanKey}],
+        },
+        {
+          focused: false,
+          selected: false,
+          path: [{_key: blockAKey}, 'children', {_key: stockTickerKey}],
+        },
+        {
+          focused: false,
+          selected: false,
+          path: [{_key: blockAKey}, 'children', {_key: barSpanKey}],
+        },
+        {
+          focused: false,
+          selected: false,
+          path: [{_key: blockBKey}, 'children', {_key: fooSpanKey}],
+        },
+        {
+          focused: false,
+          selected: false,
+          path: [{_key: blockBKey}, 'children', {_key: stockTickerKey}],
+        },
+        {
+          focused: false,
+          selected: false,
+          path: [{_key: blockBKey}, 'children', {_key: barSpanKey}],
+        },
+      ])
+    })
+
+    renderChildValues = []
+
+    const fooSpanLocator = locator.getByText('foo')
+
+    await userEvent.click(fooSpanLocator)
+
+    const firstFooSelection = {
+      anchor: {
+        path: [{_key: blockAKey}, 'children', {_key: fooSpanKey}],
+        offset: 3,
+      },
+      focus: {
+        path: [{_key: blockAKey}, 'children', {_key: fooSpanKey}],
+        offset: 3,
+      },
+      backward: false,
+    }
+
+    editor.send({
+      type: 'select',
+      at: firstFooSelection,
+    })
+
+    await vi.waitFor(() => {
+      expect(editor.getSnapshot().context.selection).toEqual(firstFooSelection)
+    })
+
+    await vi.waitFor(() => {
+      expect(
+        renderChildValues,
+        'Unexpected renderChild calls after selecting first foo span',
+      ).toEqual([
+        // After selection
+        {
+          focused: true,
+          selected: true,
+          path: [{_key: blockAKey}, 'children', {_key: fooSpanKey}],
+        },
+        {
+          focused: false,
+          selected: false,
+          path: [{_key: blockAKey}, 'children', {_key: stockTickerKey}],
+        },
+        {
+          focused: false,
+          selected: false,
+          path: [{_key: blockBKey}, 'children', {_key: stockTickerKey}],
+        },
+      ])
+    })
+
+    renderChildValues = []
+
+    await userEvent.keyboard('{ArrowRight}')
+
+    await vi.waitFor(() => {
+      expect(
+        renderChildValues,
+        'Unexpected renderChild calls after selecting first stock ticker',
+      ).toEqual([
+        {
+          focused: false,
+          selected: false,
+          path: [{_key: blockAKey}, 'children', {_key: fooSpanKey}],
+        },
+        {
+          focused: true,
+          selected: true,
+          path: [{_key: blockAKey}, 'children', {_key: stockTickerKey}],
+        },
+        {
+          focused: false,
+          selected: false,
+          path: [{_key: blockBKey}, 'children', {_key: stockTickerKey}],
+        },
+      ])
+    })
   })
 })
