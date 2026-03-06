67089e4a9b38cb5711f050c47d0fb029a9d1a828
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2026-03-06 08:20:51 +0100

    fix: remove unused internal utils

--- packages/editor/src/internal-utils/block-keys.ts
+++
@@ -1,9 +0,0 @@
-import type {PortableTextBlock} from '@portabletext/schema'
-
-export function getBlockKeys(value: Array<PortableTextBlock> | undefined) {
-  if (!value) {
-    return []
-  }
-
-  return value.map((block) => block._key)
-}
