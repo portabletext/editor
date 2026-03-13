b727b2299ee5f18f65d052e7c8ff66cf9f66631f
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2026-03-13 10:11:16 +0100

    fix: remove unused internal `insert(Text)Data` methods

--- packages/editor/src/slate-dom/plugin/with-dom.ts
+++ packages/editor/src/slate-dom/plugin/with-dom.ts
@@ -1,22 +1,6 @@
-import {applySplitNode} from '../../internal-utils/apply-split-node'
-import {
-  Element,
-  Node,
-  Path,
-  Point,
-  Range,
-  type Editor,
-  type Operation,
-  type PointRef,
-} from '../../slate'
-import {end as editorEnd} from '../../slate/editor/end'
-import {isBlock} from '../../slate/editor/is-block'
-import {isEnd} from '../../slate/editor/is-end'
+import {Path, Point, type Editor, type Operation} from '../../slate'
 import {levels} from '../../slate/editor/levels'
 import {node as editorNode} from '../../slate/editor/node'
-import {nodes} from '../../slate/editor/nodes'
-import {pointRef} from '../../slate/editor/point-ref'
-import {withoutNormalizing} from '../../slate/editor/without-normalizing'
 import {
   transformPendingPoint,
   transformPendingRange,
@@ -132,106 +116,6 @@ export const withDOM = <T extends Editor>(editor: T): T & DOMEditor => {
       const [node] = editorNode(e, path)
       e.nodeToKey.set(node, key)
     }
-  }
-
-  e.insertData = (data: DataTransfer) => {
-    e.insertTextData(data)
-  }
-
-  e.insertTextData = (data: DataTransfer): boolean => {
-    const text = data.getData('text/plain')
-
-    if (text) {
-      const lines = text.split(/\r\n|\r|\n/)
-      let split = false
-
-      for (const line of lines) {
-        if (split) {
-          withoutNormalizing(e, () => {
-            let splitAt: Point | null = null
-
-            if (e.selection) {
-              if (Range.isCollapsed(e.selection)) {
-                splitAt = e.selection.anchor
-              } else {
-                const [, end] = Range.edges(e.selection)
-                const endPointRef = pointRef(e, end)
-                e.delete({at: e.selection})
-                splitAt = endPointRef.unref()
-              }
-            }
-
-            if (!splitAt) {
-              return
-            }
-
-            const splitMatch = (n: Node) =>
-              Element.isElement(n, e.schema) && isBlock(e, n)
-
-            const beforeRef = pointRef(e, splitAt, {
-              affinity: 'backward',
-            })
-            let afterRef: PointRef | undefined
-            try {
-              const [highest] = nodes(e, {
-                at: splitAt,
-                match: splitMatch,
-                mode: 'lowest',
-                voids: false,
-              })
-
-              if (!highest) {
-                return
-              }
-
-              afterRef = pointRef(e, splitAt)
-              const depth = splitAt.path.length
-              const [, highestPath] = highest
-              const lowestPath = splitAt.path.slice(0, depth)
-              let position = splitAt.offset
-
-              for (const [node, nodePath] of levels(e, {
-                at: lowestPath,
-                reverse: true,
-                voids: false,
-              })) {
-                let didSplit = false
-
-                if (
-                  nodePath.length < highestPath.length ||
-                  nodePath.length === 0
-                ) {
-                  break
-                }
-
-                const point = beforeRef.current!
-                const isEndOfNode = isEnd(e, point, nodePath)
-
-                // always = true, so always split
-                didSplit = true
-                const properties = Node.extractProps(node, e.schema)
-                applySplitNode(e, nodePath, position, properties)
-
-                position =
-                  nodePath[nodePath.length - 1]! +
-                  (didSplit || isEndOfNode ? 1 : 0)
-              }
-
-              const point = afterRef.current || editorEnd(e, [])
-              e.select(point)
-            } finally {
-              beforeRef.unref()
-              afterRef?.unref()
-            }
-          })
-        }
-
-        e.insertText(line)
-        split = true
-      }
-      return true
-    }
-    return false
   }
 
   e.onChange = (options) => {
