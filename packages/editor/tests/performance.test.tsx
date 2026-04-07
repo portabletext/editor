9cdcdd9ed6dbba974815338f86bf656b85d26bae
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2026-04-07 12:52:54 +0200

    test: add more baseline perf tests

--- packages/editor/tests/performance.test.tsx
+++ packages/editor/tests/performance.test.tsx
@@ -5,31 +5,99 @@ import {createTestEditor} from '../src/test/vitest'
 import type {PortableTextSlateEditor} from '../src/types/slate-editor'
 
 describe('Performance', () => {
-  test('Baseline: inserting 1000 blocks', async () => {
-    const {editor, locator} = await createTestEditor()
+  describe('Baseline', () => {
+    test('Inserting 1000 blocks in an empty editor', async () => {
+      const {editor, locator} = await createTestEditor()
 
-    const start = performance.now()
+      const start = performance.now()
 
-    editor.send({
-      type: 'insert.blocks',
-      blocks: Array.from({length: 1000}, (_, i) => ({
-        _type: 'block',
-        children: [{_type: 'span', text: `b${i}`, marks: []}],
-        markDefs: [],
-        style: 'normal',
-      })),
-      placement: 'auto',
+      editor.send({
+        type: 'insert.blocks',
+        blocks: Array.from({length: 1000}, (_, i) => ({
+          _type: 'block',
+          children: [{_type: 'span', text: `b${i}`, marks: []}],
+          markDefs: [],
+          style: 'normal',
+        })),
+        placement: 'auto',
+      })
+
+      await vi.waitFor(() => {
+        expect(editor.getSnapshot().context.value.length).toBe(1000)
+
+        expect(locator.getByText('b999')).toBeInTheDocument()
+      })
+
+      const duration = performance.now() - start
+
+      console.warn(`Inserted 1000 blocks in ${duration.toFixed(2)}ms`)
     })
 
-    await vi.waitFor(() => {
-      expect(editor.getSnapshot().context.value.length).toBe(1000)
+    test('Inserting 1000 blocks before an existing block', async () => {
+      const {editor, locator} = await createTestEditor()
 
-      expect(locator.getByText('b999')).toBeInTheDocument()
+      const start = performance.now()
+
+      editor.send({
+        type: 'insert.blocks',
+        blocks: Array.from({length: 1000}, (_, i) => ({
+          _type: 'block',
+          children: [{_type: 'span', text: `b${i}`, marks: []}],
+          markDefs: [],
+          style: 'normal',
+        })),
+        placement: 'before',
+        at: {
+          anchor: {path: [{_key: 'b0'}], offset: 0},
+          focus: {path: [{_key: 'b0'}], offset: 0},
+        },
+      })
+
+      await vi.waitFor(() => {
+        expect(editor.getSnapshot().context.value.length).toBe(1001)
+
+        expect(locator.getByText('b999')).toBeInTheDocument()
+      })
+
+      const duration = performance.now() - start
+
+      console.warn(
+        `Inserted 1000 blocks before an existing block in ${duration.toFixed(2)}ms`,
+      )
     })
 
-    const duration = performance.now() - start
+    test('Inserting 1000 blocks after an existing block', async () => {
+      const {editor, locator} = await createTestEditor()
 
-    console.warn(`Inserted 1000 blocks in ${duration.toFixed(2)}ms`)
+      const start = performance.now()
+
+      editor.send({
+        type: 'insert.blocks',
+        blocks: Array.from({length: 1000}, (_, i) => ({
+          _type: 'block',
+          children: [{_type: 'span', text: `b${i}`, marks: []}],
+          markDefs: [],
+          style: 'normal',
+        })),
+        placement: 'after',
+        at: {
+          anchor: {path: [{_key: 'b0'}], offset: 0},
+          focus: {path: [{_key: 'b0'}], offset: 0},
+        },
+      })
+
+      await vi.waitFor(() => {
+        expect(editor.getSnapshot().context.value.length).toBe(1001)
+
+        expect(locator.getByText('b999')).toBeInTheDocument()
+      })
+
+      const duration = performance.now() - start
+
+      console.warn(
+        `Inserted 1000 blocks after an existing block in ${duration.toFixed(2)}ms`,
+      )
+    })
   })
 
   test('onChange is batched', async () => {
