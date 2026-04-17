831c34b5bae76577caa4d12caf84acc32053b9c6
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2026-04-17 10:22:40 +0200

    fix: stabilize span keys when writing between marks

--- packages/editor/src/behaviors/behavior.core.insert.ts
+++ packages/editor/src/behaviors/behavior.core.insert.ts
@@ -1,3 +1,5 @@
+import {isEqualMarks} from '../internal-utils/equality'
+import {getNextSpan} from '../selectors'
 import {getActiveAnnotationsMarks} from '../selectors/selector.get-active-annotation-marks'
 import {getActiveDecorators} from '../selectors/selector.get-active-decorators'
 import {getFocusSpan} from '../selectors/selector.get-focus-span'
@@ -34,19 +36,51 @@ export const coreInsertBehaviors = [
         }
       }
 
-      return {activeDecorators, activeAnnotations}
+      const activeMarks = [...activeDecorators, ...activeAnnotations]
+
+      if (isEqualMarks(focusSpan.node.marks ?? [], activeMarks)) {
+        return false
+      }
+
+      const atEndOfFocusSpan =
+        snapshot.context.selection?.focus.offset === focusSpan.node.text.length
+
+      const nextSpan = atEndOfFocusSpan ? getNextSpan(snapshot) : undefined
+
+      return {activeMarks, nextSpan}
     },
     actions: [
-      ({snapshot, event}, {activeDecorators, activeAnnotations}) => [
-        raise({
-          type: 'insert.child',
-          child: {
-            _type: snapshot.context.schema.span.name,
-            text: event.text,
-            marks: [...activeDecorators, ...activeAnnotations],
-          },
-        }),
-      ],
+      ({snapshot, event}, {activeMarks, nextSpan}) => {
+        if (nextSpan && isEqualMarks(nextSpan.node.marks, activeMarks)) {
+          return [
+            raise({
+              type: 'select',
+              at: {
+                anchor: {
+                  path: nextSpan.path,
+                  offset: 0,
+                },
+                focus: {
+                  path: nextSpan.path,
+                  offset: 0,
+                },
+              },
+            }),
+            raise(event),
+          ]
+        }
+
+        return [
+          raise({
+            type: 'insert.child',
+            child: {
+              _type: snapshot.context.schema.span.name,
+              text: event.text,
+              marks: activeMarks,
+            },
+          }),
+        ]
+      },
     ],
   }),
 ]
