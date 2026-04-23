1c59af439490890dc74943318d7f38d4e10591a8
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2026-04-23 15:40:15 +0000

    fixup! feat: add containers to editor context and expand traversal primitives

--- packages/editor/src/editor/create-slate-editor.tsx
+++ packages/editor/src/editor/create-slate-editor.tsx
@@ -59,6 +59,7 @@ export function createSlateEditor(
   buildIndexMaps(
     {
       schema: context.schema,
+      containers: slateEditor.containers,
       value: slateEditor.children,
     },
     {
