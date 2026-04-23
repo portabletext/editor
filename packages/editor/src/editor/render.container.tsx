191d0a87859aded7af1a3780297016d29d8eb03b
Christian Hamburger Grøngaard <christian.groengaard@sanity.io>
2026-04-23 15:15:33 +0000

    fixup! fix: emit minimal DOM inside editable containers

--- packages/editor/src/editor/render.container.tsx
+++ packages/editor/src/editor/render.container.tsx
@@ -2,6 +2,7 @@ import type {PortableTextBlock} from '@portabletext/schema'
 import type {ReactElement} from 'react'
 import type {ContainerConfig} from '../renderers/renderer.types'
 import type {RenderElementProps} from '../slate/react/components/editable'
+import {useSlateSelector} from '../slate/react/hooks/use-slate-selector'
 
 export function RenderContainer(props: {
   attributes: RenderElementProps['attributes']
@@ -9,9 +10,28 @@ export function RenderContainer(props: {
   element: PortableTextBlock
   containerConfig: ContainerConfig
 }) {
+  const listIndex = useSlateSelector((editor) =>
+    editor.listIndexMap.get(props.element._key),
+  )
+
+  const {element} = props
+  const textBlockAttributes =
+    'style' in element || 'listItem' in element
+      ? {
+          ...(element.style !== undefined && {'data-style': element.style}),
+          ...(element.listItem !== undefined && {
+            'data-list-item': element.listItem,
+          }),
+          ...(element.level !== undefined && {'data-level': element.level}),
+          ...(listIndex !== undefined && {'data-list-index': listIndex}),
+        }
+      : {}
+
+  const augmentedAttributes = {...props.attributes, ...textBlockAttributes}
+
   if (props.containerConfig.container.render) {
     const rendered = props.containerConfig.container.render({
-      attributes: props.attributes,
+      attributes: augmentedAttributes,
       children: props.children,
       node: props.element,
     })
@@ -21,5 +41,5 @@ export function RenderContainer(props: {
     }
   }
 
-  return <div {...props.attributes}>{props.children}</div>
+  return <div {...augmentedAttributes}>{props.children}</div>
 }
