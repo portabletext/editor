52ab44baa6c66c803021bda4d91e2cd8299a0069
Christian Grøngaard <christian.groengaard@sanity.io>
2026-06-23 08:04:29 +0000

    chore(playground): accept callouts inside table cells

--- apps/playground/src/plugins/plugin.table.tsx
+++ apps/playground/src/plugins/plugin.table.tsx
@@ -41,7 +41,7 @@ const tableContainer = defineContainer({
               {children}
             </td>
           ),
-          of: [cellImageLeaf],
+          of: [cellImageLeaf, calloutContainer],
         }),
       ],
     }),
